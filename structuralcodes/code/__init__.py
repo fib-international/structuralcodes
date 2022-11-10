881f178ad81aa1f35447b4583b509c7366a1c3d1
Morten Engen <morten.engen@multiconsult.no>
2022-11-10 20:49:47 +0100

    Re-implement functions for get and set code and NA

--- structuralcodes/code/__init__.py
+++ structuralcodes/code/__init__.py
@@ -1,12 +1,19 @@
 """Collection of functions related to design codes"""
+import types
 import typing as t
 
-from structuralcodes.core.base import DesignCode
-
-from ._code_factory import code_factory
+from . import mc2010
 
 # Global code object used by material classes
-_CODE: t.Optional[DesignCode] = None
+_CODE: t.Optional[types.ModuleType] = None
+
+# Global national annex object
+_NATIONAL_ANNEX: t.Optional[str] = None
+
+# Design code registry
+_DESIGN_CODES = {
+    'mc2010': mc2010,
+}
 
 
 def set_design_code(design_code: str) -> None:
@@ -19,23 +26,39 @@ def set_design_code(design_code: str) -> None:
         Call get_design_codes() to get a list of the available codes.
     """
     global _CODE  # pylint: disable=W0603
-    _CODE = code_factory.create(design_code.lower())
+    _CODE = _DESIGN_CODES.get(design_code.lower())
 
 
 def get_design_codes() -> t.List[str]:
     """Get a list of the available design codes."""
-    return code_factory.get_registered_codes()
+    return list(_DESIGN_CODES.keys())
 
 
 def set_national_annex(national_annex: str) -> None:
-    """Set the current national annex globally."""
-    raise NotImplementedError
+    """Set the current national annex globally.
+
+    Args:
+        national_annex (str): The abbreviation of the national annex.
+
+    Note:
+        Call get_national_annexes() on the relevant design code to see a list
+        of available national annexes.
+    """
+    global _NATIONAL_ANNEX  # pylint: disable=W0603
+    _NATIONAL_ANNEX = national_annex.lower()
 
 
 def _use_design_code(
     design_code: t.Optional[str] = None,
-) -> t.Optional[DesignCode]:
-    """Use a design code in a class"""
+) -> t.Optional[types.ModuleType]:
+    """Use a design code in a class.
+
+    Kwargs:
+        design_code (str): The abbreviation of the code.
+
+    Note:
+        Call get_design_codes() to get a list of the available codes.
+    """
     if design_code is None:
         return _CODE
-    return code_factory.create(design_code)
+    return _DESIGN_CODES.get(design_code.lower())
