ec8aeef250e0d6d991476bb9a792d25acd57fcc5
Morten Engen <morten.engen@multiconsult.no>
2022-11-16 14:11:38 +0100

    Add attributes to design code sub-package
    Co-authored-by: Daniel G. de la Morena <dgm@fhecor.es>
    Co-authored-by: talledodiego <38036285+talledodiego@users.noreply.github.com>

--- structuralcodes/code/mc2010/__init__.py
+++ structuralcodes/code/mc2010/__init__.py
@@ -1,4 +1,5 @@
 """The fib Model Code 2010"""
+import typing as t
 from ._concrete_material_properties import fcm, fctm, fctkmin, fctkmax, Gf, fcd
 
 __all__ = [
@@ -9,3 +10,7 @@ __all__ = [
     'Gf',
     'fcd',
 ]
+
+__title__: str = 'fib Model Code 2010'
+__year__: str = '2013'
+__materials__: t.Tuple[str] = ('concrete',)
