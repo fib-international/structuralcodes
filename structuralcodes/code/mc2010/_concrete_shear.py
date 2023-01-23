bc13a7871ab6553c97462e600a00ed92e3dc3c7e
Your Name <johanrbr@stud.ntnu.no>
2023-01-23 11:04:27 +0100

    added shear

---
+++ structuralcodes/code/mc2010/_concrete_shear.py
@@ -0,0 +1,17 @@
+"""A collection of shear formulas for concrete"""
+
+z = 110
+
+def vrd(vrdc: float, vrds: float) -> float:
+    """Compute the shear resistance of a web or slab.
+
+    fib Model Code 2010, Eq. (7.3-11)
+
+    Args:
+        vrdc (float): Design shear resistance of concrete.
+        vrds (float): Design shear resistance of shear reinforcement.
+
+    Returns:
+        float: Design shear resistance
+    """
+    return abs(vrdc) + abs(vrds)
