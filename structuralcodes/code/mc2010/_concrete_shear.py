17d3d19b64b2ece9c8f2e5e6106082d27483bc2d
Alexander-Wulfsberg <gregeraw@stud.ntnu.no>
2023-02-03 13:38:25 +0100

    fix

--- structuralcodes/code/mc2010/_concrete_shear.py
+++ structuralcodes/code/mc2010/_concrete_shear.py
@@ -208,7 +208,7 @@ def v_rdc(
 
     elif approx_lvl_s == 3:
         return v_rds_approx3(
-            fck, z, bw, E, As, Med, Ved, Ned, delta_e, alfa, gamma_c
+            approx_lvl_s, fck, z, bw, E, As, Med, Ved, Ned, delta_e, alfa, gamma_c
         )
 
 
@@ -582,7 +582,6 @@ def v_rd_ct(
     s_c: float,
     b_w: float,
     sigma_cp: float,
-    alfa_cp: float,
     l_x: float,
     l_bd0: float,
     S_cy: float,
@@ -608,7 +607,7 @@ def v_rd_ct(
         s_c: The first moment of area, abouve and about the centriodal axis
         b_w: The width of the cross-section at the centroidal axis
         sigma_cp: The compressive stress at centroidal axis due to prestress
-        alfa_cp: l_x/(1.2*l_bd0)
+        alfa_l: l_x/(1.2*l_bd0)
         l_x: distance between edge and point of failure (Figure: 7.3-12)
         l_bd0: follows 7.13-5
         S_cy: The first moment of area above y
@@ -628,13 +627,16 @@ def v_rd_ct(
     if approx_lvl_h == 1:
         return v_rd_ct_approx1()
 
+
 def v_rd_ct_approx1(
+    f_ctd: float,
     i_c: float,
     s_c: float,
     b_w: float,
     sigma_cp: float,
-    alfa_cp: float,
     l_x: float,
     l_bd0: float
-)-> float:
-    return 0.8*((i_c*b_w)/s_c)*(f)
+) -> float:
+    """Using """
+    alfa_l = l_x/(1.2*l_bd0)
+    return 0.8*((i_c*b_w)/s_c)*((f_ctd**2)*+alfa_l*sigma_cp*f_ctd)**0.5
