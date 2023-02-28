3e9b6ea3cf7426ff62c76c0f6e16b28dd8668054
her_username <git-commit-address>
2023-03-01 00:27:55 +0100

    i_tried

--- structuralcodes/codes/mc2010/_concrete_torsion.py
+++ structuralcodes/codes/mc2010/_concrete_torsion.py
@@ -1,8 +1,10 @@
 "Covers torsion in Model code 2010, 7.3.4"
 
 from math import pi, tan, sin, cos
-from structuralcodes.codes.mc2010 import _concrete_shear
-from structuralcodes.codes.mc2010._concrete_shear import v_rd_max
+
+# import _concrete_shear as cs
+from _concrete_shear import epsilon_x
+from _concrete_shear import v_rd_max
 
 
 def v_ed_ti(t_ed: float, a_k: float, z_i: float):
@@ -58,22 +60,21 @@ def t_rd_max(
         The maximum allowed torsion allowed
     """
     t_ef = d_k / 8
-    nfc = min((30 / f_ck) ** (1 / 3), 1)
 
     if approx_lvl == 3:
         k_epsilon = 0.55
     elif approx_lvl == 4:
-        epsilon_1 = _concrete_shear.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + (
-            epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + 0.002
+        epsilon_1 = cs.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + (
+            cs.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + 0.002
         ) * ((1 / tan(theta * pi / 180)) ** 2)
         k_epsilon = min(1 / (1.2 + 55 * epsilon_1), 0.65)
     elif approx_lvl == 5:
-        theta_min = 20 + 10000 * epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e)
-        epsilon_1 = epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + (
-            epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + 0.002
+        theta_min = 20 + 10000 * cs.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e)
+        epsilon_1 = cs.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + (
+            cs.epsilon_x(E_s, As, Med, Ved, Ned, z, delta_e) + 0.002
         ) * ((1 / tan(theta_min * pi / 180)) ** 2)
         k_epsilon = min(1 / (1.2 + 55 * epsilon_1), 0.65)
-    k_c = nfc * k_epsilon
+    k_c = cs.eta_fc(f_ck) * k_epsilon
     result = (
         k_c
         * f_ck
@@ -154,7 +155,7 @@ def t_rd(
         ** 2
         + (
             Ved
-            / v_rd_max(
+            / cs.v_rd_max(
                 approx_lvl,
                 fck,
                 bw,
@@ -174,3 +175,5 @@ def t_rd(
         <= 1
     )
     return check
+
+print(t_rd_max(35, 150, 50000, 40, 1, 200000, 2000, 0, 2000, 0, 180, 20, 1.5))
