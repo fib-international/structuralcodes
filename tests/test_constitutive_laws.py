385d8348c9a8100cc051565bcdc890372f791ce6
talledodiego <38036285+talledodiego@users.noreply.github.com>
2023-08-08 18:41:34 +0200

    Fix format
    Fix format on first test for passing black

--- tests/test_constitutive_laws.py
+++ tests/test_constitutive_laws.py
@@ -9,8 +9,7 @@ from structuralcodes.materials.constitutive_laws import ElasticPlastic
 
 @pytest.mark.parametrize(
     'E, strain, expected',
-    [(210000, 0.003, 210000*0.003),
-     (200000, 0.002, 200000*0.002)],
+    [(210000, 0.003, 210000 * 0.003), (200000, 0.002, 200000 * 0.002)],
 )
 def test_elastic_floats(E, strain, expected):
     """Test the elastic material"""
@@ -20,12 +19,14 @@ def test_elastic_floats(E, strain, expected):
 
 @pytest.mark.parametrize(
     'E, fy, strain, expected',
-    [(210000, 410, 0.001, 210.),
-     (210000, 410, 0.003, 410.),
-     (210000, 410, 0.010, 410.),
-     (200000, 450, 0.002, 400.),
-     (200000, 450, 0.004, 450.),
-     (200000, 450, 0.010, 450.)],
+    [
+        (210000, 410, 0.001, 210.0),
+        (210000, 410, 0.003, 410.0),
+        (210000, 410, 0.010, 410.0),
+        (200000, 450, 0.002, 400.0),
+        (200000, 450, 0.004, 450.0),
+        (200000, 450, 0.010, 450.0),
+    ],
 )
 def test_elasticplastic_floats(E, fy, strain, expected):
     """Test the elasticPlastic material"""
