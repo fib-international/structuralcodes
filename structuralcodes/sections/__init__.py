06fff19831f44ae45e18f6a2312d3e7be4eb0c72
Morten Engen <morten.engen@multiconsult.no>
2024-01-30 21:41:25 +0100

    Define public API of sections sub-package

--- structuralcodes/sections/__init__.py
+++ structuralcodes/sections/__init__.py
@@ -1,11 +1,29 @@
 """Main entry point for sections."""
+from ._generic import GenericSection, GenericSectionCalculator
+from ._geometry import (
+    Geometry,
+    PointGeometry,
+    SurfaceGeometry,
+    CompoundGeometry,
+)
+from ._geometry import add_reinforcement, add_reinforcement_line
+from ._section_integrators import (
+    SectionIntegrator,
+    MarinIntegrator,
+    FiberIntegrator,
+)
+
 
 __all__ = [
     'GenericSection',
     'GenericSectionCalculator',
+    'Geometry',
     'PointGeometry',
     'SurfaceGeometry',
     'CompoundGeometry',
     'add_reinforcement',
+    'add_reinforcement_line',
+    'SectionIntegrator',
+    'FiberIntegrator',
     'MarinIntegrator',
 ]
