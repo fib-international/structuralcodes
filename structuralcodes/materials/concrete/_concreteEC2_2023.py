03e5faa55acb6fa1c20ad4d8eca06801d2794140
DanielGMorenaFhecor <dgm@fhecor.es>
2024-04-04 14:15:08 +0200

    concrete ec2 2023 tests

--- structuralcodes/materials/concrete/_concreteEC2_2023.py
+++ structuralcodes/materials/concrete/_concreteEC2_2023.py
@@ -7,8 +7,6 @@ from structuralcodes.codes import ec2_2023
 from ._concrete import Concrete
 
 
-# TODO: complete the documentation and discuss if properties/functions
-# are considered proper for the framework
 class ConcreteEC2_2023(Concrete):  # noqa: N801
     """Concrete implementation for EC2 2023 Concrete."""
 
@@ -21,6 +19,10 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
     _fcm: t.Optional[float] = None
     _fctm: t.Optional[float] = None
     _Ecm: t.Optional[float] = None
+    _fctk_5: t.Optional[float] = None
+    _fctk_95: t.Optional[float] = None
+    _eps_c1: t.Optional[float] = None
+    _eps_cu1: t.Optional[float] = None
 
     def __init__(
         self,
@@ -61,6 +63,12 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
                 + f'{strength_dev_class} not {valid_dev_classes}'
             )
 
+        # Check for valid security coeff
+        if gamma_C <= 0:
+            raise ValueError(
+                f'gamma_C cannot be less than zero. Current: {gamma_C}'
+            )
+
         super().__init__(
             fck=fck, name=name, density=density, existing=existing
         )
@@ -68,12 +76,16 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
         self._strength_dev_class = strength_dev_class
         self._gamma_C = gamma_C
 
-    # TODO: implement this method
     def _reset_attributes(self):
+        """Reset computed properties."""
         # We only need to reset computed properties
         self._fcm = None
         self._fctm = None
         self._Ecm = None
+        self._fctk_5 = None
+        self._fctk_95 = None
+        self._eps_c1 = None
+        self._eps_cu1 = None
 
     @property
     def fcm(self) -> float:
@@ -115,9 +127,7 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
         Returns:
             float: the mean concrete tensile strength.
         """
-        if self._fctm is not None:
-            return self._fctm
-        self._fctm = ec2_2023.fctm(self.fck)
+        self._fctm = self._fctm or ec2_2023.fctm(self.fck)
         return self._fctm
 
     @fctm.setter
@@ -132,30 +142,53 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
 
     @property
     def fctk_5(self) -> float:
-        return ec2_2023.fctk_5(self.fctm)
+        """Returns the 5% mean concrete tensile strength fractile.
+
+        Returns:
+            float: the 5% mean concrete tensile strength fractile in MPa
+        """
+        self._fctk_5 = self._fctk_5 or ec2_2023.fctk_5(self.fctm)
+        return self._fctk_5
 
     @property
     def fctk_95(self) -> float:
-        return ec2_2023.fctk_95(self.fctm)
+        """Returns the 95% mean concrete tensile strength fractile.
+
+        Returns:
+            float: the 5% mean concrete tensile strength fractile in MPa
+        """
+        self._fctk_95 = self._fctk_95 or ec2_2023.fctk_95(self.fctm)
+        return self._fctk_95
 
     @property
     def Ecm(self) -> float:
-        if self._Ecm is not None:
-            return self._Ecm
-        self._Ecm = ec2_2023.Ecm(self.fcm, self._kE)
+        """Returns the secant modulus.
+
+        Returns:
+            float: the secant concrete modulus in MPa
+        """
+        self._Ecm = self._Ecm or ec2_2023.Ecm(self.fcm, self._kE)
         return self._Ecm
 
     @Ecm.setter
     def Ecm(self, value: float) -> None:
+        """Sets the secant modulus.
+
+        Args:
+            float: the secand modulus value in MPa.
+        """
         self._Ecm = value
 
-    def fcd(self, t_ref: float, t0: float, fck_ref: float = 40) -> float:
+    def fcd(
+        self, t_ref: float = 28, t0: float = 91, fck_ref: float = 40
+    ) -> float:
         """Computes the value of the design compressive strength of
         concrete.
 
         Args:
-            t_ref (float): the reference time in days
-            t0 (float): age at loading in days
+            t_ref (float,optional): the reference time in days
+                (default is 28 days).
+            t0 (float, optional): age at loading in days (default is 91 days).
             fck_ref (float, optional): the reference compressive strength
                 MPa (default is 40 MPa).
 
@@ -171,11 +204,12 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
         k_tc = ec2_2023.k_tc(t_ref, t0, self._strength_dev_class)
         return ec2_2023.fcd(self.fck, eta_cc, k_tc, self._gamma_C)
 
-    def fctd(self, t_ref: float) -> float:
+    def fctd(self, t_ref: float = 28) -> float:
         """Computes the value of the design tensile strength of concrete.
 
         Args:
-            t_ref (float): the reference time in days
+            t_ref (float, optional): the reference time in days
+                (default is 28 days).
 
         Returns:
             float: the design tensile strength of concrete in MPa
@@ -183,19 +217,45 @@ class ConcreteEC2_2023(Concrete):  # noqa: N801
         Raises:
             ValueError: if t_ref is less than 0
         """
-        k_tt = ec2_2023.k_tt(t_ref=t_ref)
+        k_tt = ec2_2023.k_tt(
+            t_ref=t_ref, strength_dev_class=self._strength_dev_class
+        )
         return ec2_2023.fctd(self.fctk_5, k_tt, self._gamma_C)
 
     @property
     def eps_c1(self) -> float:
-        return ec2_2023.eps_c1(self.fcm)
+        """Returns the strain at maximum compressive strength
+            of concrete (fcm) for the Sargin constitutive law.
+
+        Returns:
+            float: the strain at maximum compressive strength of concrete
+        """
+        self._eps_c1 = self._eps_c1 or ec2_2023.eps_c1(self.fcm)
+        return self._eps_c1
 
     @property
     def eps_cu1(self) -> float:
-        return ec2_2023.eps_cu1(self.fcm)
+        """Returns the strain at concrete failure of concrete.
 
-    @property
-    def sigma_c(self) -> float:
+        Returns:
+            float: the maximum strength at failure of concrete
+        """
+        self._eps_cu1 = self._eps_cu1 or ec2_2023.eps_cu1(self.fcm)
+        return self._eps_cu1
+
+    def sigma_c(self, eps_c: float) -> float:
+        """Computes the compressive stress of concrete given a
+        strain eps_c under short term uniaxial compression.
+
+        Args:
+            eps_c (float): the strain of concrete
+
+        Returns:
+            float: the compressive stress of concrete in MPa
+
+        Raises:
+            ValueError: if eps_c is less than 0
+        """
         return ec2_2023.sigma_c(
-            self.Ecm, self.fcm, self.eps_c, self.eps_c1, self.eps_cu1
+            self.Ecm, self.fcm, eps_c, self.eps_c1, self.eps_cu1
         )
--- tests/test_concrete_ec2_2023.py
+++ tests/test_concrete_ec2_2023.py
@@ -1,3 +1,172 @@
 """Test module for testing the EC2_2023 Concrete."""
 
-# TODO: design the tests for the implementation of the concrete class.
+import math
+
+import pytest
+
+from structuralcodes.materials.concrete import ConcreteEC2_2023
+
+
+@pytest.fixture
+def default_concrete():
+    """Returns default concrete."""
+    return ConcreteEC2_2023(fck=30)
+
+
+def test_initialization(default_concrete):
+    """Test for initialization and attribute setting."""
+    assert default_concrete.fck == 30
+    assert default_concrete.name.startswith('C')
+    assert default_concrete.density == 2400.0
+    assert default_concrete._kE == 9500
+    assert default_concrete._strength_dev_class == 'CN'
+    assert default_concrete._gamma_C == 1.5
+
+
+def test_invalid_strength_dev_class():
+    """Test for invalid strength development class."""
+    with pytest.raises(ValueError):
+        ConcreteEC2_2023(fck=30, strength_dev_class='invalid_class')
+
+
+def test_invalid_gamma_C():
+    """Test for invalid sec. coefficient."""
+    with pytest.raises(ValueError):
+        ConcreteEC2_2023(fck=30, gamma_C=-1)
+
+
+def test_fcm_property(default_concrete):
+    """Test for fcm property."""
+    expected_fcm = default_concrete.fck + 8
+    assert default_concrete.fcm == expected_fcm
+
+
+def test_fcm_setter_invalid_value(default_concrete):
+    """Test for fcm setter with invalid value."""
+    with pytest.raises(ValueError):
+        default_concrete.fcm = default_concrete.fck - 1
+
+
+def test_fcd_default_parameters(default_concrete):
+    """Test for fcd method with default parameters."""
+    expected_fcd_default = 20
+    assert default_concrete.fcd() == expected_fcd_default
+
+
+def test_fcd_custom_parameters(default_concrete):
+    """Test for fcd method with custom parameters."""
+    custom_t_ref = 15
+    custom_t0 = 70
+    custom_fck_ref = 30
+    expected_fcd_custom = 17
+    assert (
+        default_concrete.fcd(
+            t_ref=custom_t_ref, t0=custom_t0, fck_ref=custom_fck_ref
+        )
+        == expected_fcd_custom
+    )
+
+
+def test_fcd_invalid_parameters(default_concrete):
+    """Test for fcd method with invalid parameters."""
+    invalid_t_ref = -1
+    with pytest.raises(ValueError):
+        default_concrete.fcd(t_ref=invalid_t_ref)
+
+
+def test_fctm_property(default_concrete):
+    """Test for fctm property."""
+    expected_fctm = 2.896
+    assert math.isclose(default_concrete.fctm, expected_fctm, rel_tol=0.001)
+
+
+def test_fctm_setter(default_concrete):
+    """Test for custom fctm value."""
+    custom_fctm = 3.5
+    default_concrete.fctm = custom_fctm
+    assert default_concrete._fctm == custom_fctm
+
+
+def test_fctk_5_property(default_concrete):
+    """Test for fctk_5 property."""
+    expected_fctk_5 = 2.028
+    assert math.isclose(
+        default_concrete.fctk_5, expected_fctk_5, rel_tol=0.001
+    )
+
+
+def test_fctk_95_property(default_concrete):
+    """Test for fctk_95 property."""
+    expected_fctk_95 = 3.765
+    assert math.isclose(
+        default_concrete.fctk_95, expected_fctk_95, rel_tol=0.001
+    )
+
+
+def test_Ecm_property_and_setter(default_concrete):
+    """Test for Ecm property and its setter."""
+    expected_Ecm = 31938.766
+    assert math.isclose(default_concrete.Ecm, expected_Ecm, rel_tol=0.001)
+    custom_Ecm = 35000  # Test the override of Ecm
+    default_concrete.Ecm = custom_Ecm
+    assert default_concrete._Ecm == custom_Ecm
+
+
+def test_fctd_default_parameter(default_concrete):
+    """Test for fctd method with default parameter."""
+    expected_fctd_default = 1.081
+    assert math.isclose(
+        default_concrete.fctd(), expected_fctd_default, rel_tol=0.001
+    )
+
+
+def test_fctd_custom_parameter(default_concrete):
+    """Test for fctd method with custom parameter."""
+    custom_t_ref = 45
+    expected_fctd_custom = 0.9461
+    assert math.isclose(
+        default_concrete.fctd(t_ref=custom_t_ref),
+        expected_fctd_custom,
+        rel_tol=0.001,
+    )
+
+
+def test_fctd_invalid_parameter(default_concrete):
+    """Test for fctd method with invalid parameter."""
+    invalid_t_ref = -5
+    with pytest.raises(ValueError):
+        default_concrete.fctd(t_ref=invalid_t_ref)
+
+
+def test_eps_c1_property(default_concrete):
+    """Test for eps_c1 property."""
+    expected_eps_c1 = 0.002353
+    assert math.isclose(
+        default_concrete.eps_c1, expected_eps_c1, rel_tol=0.001
+    )
+
+
+def test_eps_cu1_property(default_concrete):
+    """Test for eps_cu1 property."""
+    expected_eps_cu1 = 0.0035
+    assert math.isclose(
+        default_concrete.eps_cu1, expected_eps_cu1, rel_tol=0.001
+    )
+
+
+def test_sigma_c_with_valid_strain(default_concrete):
+    """Test for sigma_c method with valid strain."""
+    valid_strain = 0.001
+    expected_sigma_c = 25.830
+    assert math.isclose(
+        default_concrete.sigma_c(eps_c=valid_strain),
+        expected_sigma_c,
+        rel_tol=0.001,
+    )
+
+
+def test_sigma_c_with_invalid_strain(default_concrete):
+    """Test for sigma_c method with invalid strain (negative)."""
+    invalid_strain = -0.001
+    with pytest.raises(ValueError):
+        default_concrete.sigma_c(eps_c=invalid_strain)
