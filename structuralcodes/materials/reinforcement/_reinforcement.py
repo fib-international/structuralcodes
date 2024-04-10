e3662705a52bea917f28d5aa17b25e4967bd093b
Morten Engen <morten.engen@multiconsult.no>
2024-04-10 22:36:12 +0200

    Add Youngs modulus to reinforcement base class

--- structuralcodes/materials/reinforcement/_reinforcement.py
+++ structuralcodes/materials/reinforcement/_reinforcement.py
@@ -9,15 +9,21 @@ class Reinforcement(Material):
     """The abstract reinforcement material."""
 
     _fyk: float
+    _Es: float
 
     def __init__(
-        self, fyk: float, density: float, name: t.Optional[str] = None
+        self,
+        fyk: float,
+        Es: float,
+        density: float,
+        name: t.Optional[str] = None,
     ) -> None:
         """Initializes an abstract reinforcement material."""
         name = name if name is not None else 'Reinforcement'
         super().__init__(density, name)
 
         self._fyk = abs(fyk)
+        self._Es = abs(Es)
 
     @property
     def fyk(self) -> float:
@@ -28,3 +34,13 @@ class Reinforcement(Material):
     def fyk(self, fyk: float) -> None:
         """Setter for fyk (in MPa)."""
         self._fck = abs(fyk)
+
+    @property
+    def Es(self) -> float:
+        """Returns Es in MPa."""
+        return self._Es
+
+    @Es.setter
+    def Es(self, Es: float) -> None:
+        """Setter for Es (in MPa)."""
+        self._Es = abs(Es)
