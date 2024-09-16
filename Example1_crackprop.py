7af330487e111174b3872bb8964a93b4e2b8cf59
Tu Nombre <tuemail@example.com>
2024-09-16 15:51:37 +0200

    cracked_propreties of reinforced concrete

--- Example1_crackprop.py
+++ Example1_crackprop.py
@@ -1,11 +1,8 @@
-import math
-
 from shapely import Polygon
 from shapely.geometry import Polygon
 
 from structuralcodes import codes, materials
-from structuralcodes.geometry import CompoundGeometry, SurfaceGeometry
-from structuralcodes.plots.section_plots import draw_section
+from structuralcodes.geometry import SurfaceGeometry
 from structuralcodes.sections._generic import GenericSection
 from structuralcodes.sections._reinforcement import add_reinforcement_line
 
@@ -41,69 +38,3 @@ sec.geometry._reinforced_concrete = (
 
 print(sec.cracked_properties)
 # print(sec.cracked_properties.i_yy_2 / sec.gross_properties.i11)
-
-
-def cracked_properties(section: GenericSection, n, neg_bending=False):
-    """TODO."""
-    if not section.geometry.reinforced_concrete:
-        return None
-
-    if neg_bending:
-        r = section.section_calculator.calculate_bending_strength(theta=0, n=n)
-        curv = -r.chi_y / 10  # small curvature to get n.a
-    else:
-        r = section.section_calculator.calculate_bending_strength(
-            theta=math.pi, n=n
-        )
-        curv = r.chi_y / 10  # small curvature to get n.a
-
-    eps = section.section_calculator.find_equilibrium_fixed_curvature(
-        section.geometry, n, curv, 0
-    )[0]
-    y = -eps / curv  # distance to neutral fibre
-
-    def create_surface_geometries(polygons_list, material):
-        """Process shapely polygons to SurfaceGeometries."""
-        # Create an empty list to store SurfaceGeometry objects
-        surface_geometries = []
-
-        # Iterate over the list of polygons and create a SurfaceGeometry for each one
-        for polygon in polygons_list:
-            # Create a new SurfaceGeometry for the current polygon
-            surface_geometry = SurfaceGeometry(polygon, material)
-            # Add the new SurfaceGeometry to the list
-            surface_geometries.append(surface_geometry)
-
-        return surface_geometries
-
-    cut_geom = CompoundGeometry(None, None)
-    for i, part in enumerate(section.geometry.geometries):
-        min_x, max_x, min_y, max_y = part.calculate_extents()
-        above_div, below_div = part.split(((min_x, y), 0))
-        if neg_bending:
-            subpart_poly = below_div
-        else:
-            subpart_poly = above_div
-        # convert to SurfaceGeometry
-        subpart_sg = create_surface_geometries(subpart_poly, part.material)
-        if True:
-            if i == 0:
-                cut_geom = CompoundGeometry(subpart_sg)
-            else:
-                cut_geom += subpart_sg
-    for reinf in section.geometry.point_geometries:
-        cut_geom += reinf
-    cracked_sec = GenericSection(cut_geom)
-    draw_section(cracked_sec)
-    # draw_section_response(cracked_sec, eps, curv)
-    # cracked_sec = cut_generic_section_upper_part(section, x)
-    return cracked_sec.gross_properties
-
-
-"""print('----------')
-res = cracked_properties(sec, 0, True)
-print(res)
-print('----------')
-res = cracked_properties(sec, 0, False)
-print(res)
-print('----------')"""
