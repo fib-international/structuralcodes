82200fd3200a12f2e736f3f728edf64f9c8734d2
Morten Engen <morten.engen@multiconsult.no>
2024-10-15 21:57:45 +0200

    Factor out creep functions

--- structuralcodes/codes/ec2_2004/annex_b_shrink_and_creep.py
+++ structuralcodes/codes/ec2_2004/annex_b_shrink_and_creep.py
@@ -7,7 +7,11 @@ import typing as t
 import numpy as np
 import numpy.typing as npt
 
-ALPHA_CEMENT_DICT = {'R': 1, 'N': 0, 'S': -1}
+from structuralcodes.codes.mc2010._concrete_creep_and_shrinkage import (
+    t_T as t_T_mc2010,
+)
+
+ALPHA_CEMENT_DICT = {'R': 1.0, 'N': 0.0, 'S': -1.0}
 ALPHA_DS_DICT = {
     'R': {'alpha_ds1': 6, 'alpha_ds2': 0.11},
     'N': {'alpha_ds1': 4, 'alpha_ds2': 0.12},
@@ -15,58 +19,114 @@ ALPHA_DS_DICT = {
 }
 
 
-def phi(
-    h_0: float,
-    f_cm: float,
-    RH: int = 50,
-    cement_class: t.Literal['R', 'N', 'S'] = 'R',
-    t0: float = 7,
-    t: npt.ArrayLike = 18263,
-) -> float:
-    """Calculates the creep number.
+def phi(phi_0: float, beta_c: npt.ArrayLike) -> npt.ArrayLike:
+    """Calculate the creep number.
 
     EN 1992-1-1:2004, Eq. (B.1).
 
     Args:
-        h_0 (float): The product of 2 times the cross-sectional area divided by
-            the exposed circumference according to (B.6).
-        f_cm (float): The mean concrete strength,
-        RH (int): The relative humidity in percent, defaults to 50%.
-
-    Keyword Args:
-        cement_class (str): The cement class, defaults to 'R'. Possible values:
-            'R', 'N', 'S',
-        t0 (float): The age of the concrete at the time (in days) of loading.
-        t (ArrayLike): The age of the concrete at the time (in days) of
-            evaluation (50 years default).
+        phi_0 (float): The standardized creep number defined in Eq. B.2.
+        beta_c (npt.ArrayLike): A factor taking into account the creep
+            development as a function of time after loading defined in Eq.
+            (B.7).
 
     Returns:
-        float: The creep value for the load, phi(t, t0).
-
-    Raises:
-        ValueError: checks if the cement class equals R, N or S.
+        float: The creep number.
     """
-    # The cement class will decide the value of alpha_cement, ds1 and ds2.
-    # The values are given in (B.9) and (B.12)
+    return phi_0 * beta_c
 
-    _cement_class = cement_class.upper().strip()
-    alpha_cement = ALPHA_CEMENT_DICT.get(_cement_class)
-    if alpha_cement is None:
-        raise ValueError(f'cement_class={cement_class}, expected R, N or S')
 
-    _beta_H = beta_H(h_0, f_cm, RH)
-    _phi_RH = phi_RH(h_0, f_cm, RH)
-    _beta_fcm = beta_fcm(f_cm)
-    _t0_adj = t0_adj(t0, alpha_cement)
-    beta_t0 = 1 / (0.1 + _t0_adj**0.20)  # (B.5)
-    _beta_c = beta_c(t0, t, _beta_H)
+def phi_0(phi_RH: float, beta_fcm: float, beta_t0: float) -> float:
+    """Calculate the standardized creep number.
 
-    phi_0 = _phi_RH * _beta_fcm * beta_t0  # (B.2)
-    return phi_0 * _beta_c
+    EN 1992-1-1:2004, Eq. (B.2).
+
+    Args:
+        phi_RH (float): The effect of relative humidity defined in Eq. B.3.
+        beta_fcm (float): The effect of the concrete strength defined in Eq.
+            B.4.
+        beta_t0 (float): The effect of the age at loading defined in Eq. B.5.
+
+    Returns:
+        float: The standardized creep number.
+    """
+    return phi_RH * beta_fcm * beta_t0
+
+
+def phi_RH(
+    h_0: float, fcm: float, RH: float, alpha_1: float, alpha_2: float
+) -> float:
+    """Calculates the effect of relative humidity on the standardized creep
+    number.
+
+    EN 1992-1-1:2004, Eq. (B.3).
+
+    Args:
+        h_0 (float): The effective cross sectional thickness, Equation (B.6).
+        fcm (float): The mean concrete strength in MPa.
+        RH (float): The relative humidity in percent.
+        alpha_1 (float): A factor describing the effect of concrete strength
+            defined in Eq. (B.8c).
+        alpha_2 (float): A factor describing the effect of concrete strength
+            defined in Eq. (B.8c).
+
+    Returns:
+        float: The calculation parameter (B.3).
+    """
+    if fcm <= 35:
+        return 1 + (1 - RH / 100) / (0.1 * h_0 ** (1 / 3))
+    return (1 + (1 - RH / 100) / (0.1 * h_0 ** (1 / 3)) * alpha_1) * alpha_2
+
+
+def beta_fcm(fcm: float) -> float:
+    """Calculates the effect of the concrete strength on the standardized creep
+    number.
+
+    EN 1992-1-1:2004, Eq. (B.4).
+
+    Args:
+        fcm (float): The mean concrete strength in MPa.
+
+    Returns:
+        float: The effect of concrete strength.
+    """
+    return 16.8 / fcm**0.5
+
+
+def beta_t0(t0: float) -> float:
+    """Calculates the effect of age at loading on the standardized creep
+    number.
+
+    EN 1992-1-1:2004, Eq. (B.5).
+
+    Args:
+        t0 (float): The age at loading in days.
+
+    Returns:
+        float: The effect of age at loading.
+    """
+    return 1 / (0.1 + t0**0.20)
+
+
+def h_0(Ac: float, u: float) -> float:
+    """Calculates the effective thickness of the cross section.
+
+    EN 1992-1-1:2004, Eq. (B.6).
+
+    Args:
+        Ac (float): The cross section area.
+        u (float): The part of the circumference of the cross section subject
+            to drying.
+
+    Returns:
+        float: The effective thickness.
+    """
+    return 2 * Ac / u
 
 
 def beta_c(t0: float, t: npt.ArrayLike, beta_H: float) -> float:
-    """Calculates the factor beta_c.
+    """Calculates the factor that describes the creep development as a function
+    of time after loading.
 
     EN 1992-1-1:2004, Eq. (B.7).
 
@@ -81,6 +141,66 @@ def beta_c(t0: float, t: npt.ArrayLike, beta_H: float) -> float:
     t_load = np.atleast_1d(t - t0)
     t_load[t_load < 0.0] = 0.0
     return (t_load / (beta_H + t_load)) ** 0.3
+
+
+def beta_H(h_0: float, fcm: float, RH: float, alpha_3) -> float:
+    """Calculates the effect of relative humidity and the effective thickness
+    of the structural element.
+
+    EN 1992-1-1:2004, Eq. (B.8a and b).
+
+    Args:
+        h_0 (float): The effective cross sectional thickness, Equation (B.6).
+        fcm (float): The mean concrete strength in MPa.
+        RH (float): The relative humidity in percent.
+        alpha_3 (float): A factor describing the effect of concrete strength
+            defined in Eq. B.8c.
+
+    Returns:
+        float: The effect of humidity and the effective thickness of the
+        element.
+    """
+    if fcm <= 35:
+        # (B.8a)
+        return min(1.5 * (1 + (0.012 * RH) ** 18) * h_0 + 250, 1500)
+    # (B.8b)
+    return min(
+        1.5 * (1 + (0.012 * RH) ** 18) * h_0 + 250 * alpha_3,
+        1500 * alpha_3,
+    )
+
+
+def alpha_1(fcm: float) -> float:
+    """A factor taking into account the effect of concrete strength.
+
+    EN 1992-1-1:2004, Eq. (B.8c).
+
+    Args:
+        fcm (float): The mean concrete strength in MPa.
+    """
+    return (35 / fcm) ** 0.7
+
+
+def alpha_2(fcm: float) -> float:
+    """A factor taking into account the effect of concrete strength.
+
+    EN 1992-1-1:2004, Eq. (B.8c).
+
+    Args:
+        fcm (float): The mean concrete strength in MPa.
+    """
+    return (35 / fcm) ** 0.2
+
+
+def alpha_3(fcm: float) -> float:
+    """A factor taking into account the effect of concrete strength.
+
+    EN 1992-1-1:2004, Eq. (B.8c).
+
+    Args:
+        fcm (float): The mean concrete strength in MPa.
+    """
+    return (35 / fcm) ** 0.5
 
 
 def t0_adj(t0: float, alpha_cement: float) -> float:
@@ -98,68 +218,51 @@ def t0_adj(t0: float, alpha_cement: float) -> float:
     return max(t0 * (9 / (2 + t0**1.2) + 1) ** alpha_cement, 0.5)
 
 
-def beta_fcm(f_cm: float) -> float:
-    """Calculates beta_f_cm.
+def t_T(
+    t: npt.ArrayLike, T_cur: npt.ArrayLike, dt: npt.ArrayLike = None
+) -> npt.ArrayLike:
+    """Calculates the maturity of the concrete.
 
-    EN 1992-1-1:2004, Eq. (B.4).
-
-    Args:
-        f_cm (float): The mean concrete strength.
-
-    Returns:
-        float: The factor defined in Equation (B.4).
-    """
-    return 16.8 / f_cm**0.5
-
-
-def phi_RH(h_0: float, f_cm: float, RH: float) -> float:
-    """Calculates phi_RH.
-
-    EN 1992-1-1:2004, Eq. (B.3).
+    EN 1992-1-1:2004, Eq. (B.10).
 
     Args:
-        h_0 (float): The effective cross sectional thickness, Equation (B.6).
-        f_cm (float): The mean concrete strength.
-        RH (float): The relative humidity in percent.
+        t (npt.ArrayLike): The age of the concrete in days.
+        T_cur (npt.ArrayLike): The temperature of the environment during curing
+            in degrees Celcius.
+
+    Keyword Args:
+        dt (npt.ArrayLike): Number of days at which T_cur prevails. Required
+            when providing a list for T_cur.
 
     Returns:
-        float: The calculation parameter (B.3).
+        np.ndarray: The temperature corrected age of the concrete in days at
+        loading.
     """
-    # (B.8c) Alpha 1 to 2 is a constant where the only variable is f_cm
-    alpha_1, alpha_2 = (
-        (35 / f_cm) ** 0.7,
-        (35 / f_cm) ** 0.2,
-    )
-
-    if f_cm <= 35:
-        return 1 + (1 - RH / 100) / (0.1 * h_0 ** (1 / 3))
-    return (1 + (1 - RH / 100) / (0.1 * h_0 ** (1 / 3)) * alpha_1) * alpha_2
+    return t_T_mc2010(t0=t, T_cur=T_cur, dt=dt)
 
 
-def beta_H(h_0: float, f_cm: float, RH: float) -> float:
-    """Calculates beta_H.
-
-    EN 1992-1-1:2004, Eq. (B.8a and b).
+def alpha_cement(cement_class: t.Literal['S', 'N', 'R']) -> float:
+    """An exponent that depends on the cement type.
 
     Args:
-        h_0 (float): The effective cross sectional thickness, Equation (B.6).
-        f_cm (float): The mean concrete strength.
-        RH (float): The relative humidity in percent.
+        cement_class (str): The cement class, either 'S', 'N' or 'R'.
 
     Returns:
-        float: The calculation parameter defined in (B.8).
-    """
-    # (B.8c) Alpha 3 is a constant where f_cm is the only  variable
-    alpha_3 = (35 / f_cm) ** 0.5
+        float: The exponent that depends on the cement type.
 
-    if f_cm <= 35:
-        # (B.8a) and (B.3a) applies
-        return min(1.5 * (1 + (0.012 * RH) ** 18) * h_0 + 250, 1500)
-    # (B.8b) and (B.3b) applies
-    return min(
-        1.5 * (1 + (0.012 * RH) ** 18) * h_0 + 250 * alpha_3,
-        1500 * alpha_3,
-    )
+    Raises:
+        ValueError: If an invalid cement class is provided.
+    """
+    _alpha_cement = ALPHA_CEMENT_DICT.get(cement_class.upper())
+
+    if _alpha_cement is None:
+        raise ValueError(
+            (
+                f'"{cement_class}" is not a valid cement class. '
+                'Use either S, N or R.'
+            )
+        )
+    return _alpha_cement
 
 
 def eps_cs(
