dbf795f984594996a28184bc5e1660161889e8b1
Morten Engen <morten.engen@multiconsult.no>
2024-10-17 06:55:28 +0200

    Clean up docstrings

--- structuralcodes/codes/ec2_2004/annex_b_shrink_and_creep.py
+++ structuralcodes/codes/ec2_2004/annex_b_shrink_and_creep.py
@@ -2,6 +2,8 @@
 Annex B.
 """
 
+from __future__ import annotations  # To have clean hints of ArrayLike in docs
+
 import typing as t
 
 import numpy as np
@@ -16,7 +18,7 @@ ALPHA_DS_DICT = {
 
 
 def eps_cs(eps_cd: npt.ArrayLike, eps_ca: npt.ArrayLike) -> npt.ArrayLike:
-    """Calculates the total shrinkage strain.
+    """Calculate the total shrinkage strain.
 
     EN 1992-1-1:2004, Eq. (3.8).
 
@@ -26,6 +28,9 @@ def eps_cs(eps_cd: npt.ArrayLike, eps_ca: npt.ArrayLike) -> npt.ArrayLike:
 
     Returns:
         npt.ArrayLike: The total shrinkage strain.
+
+    Note:
+        In EC2 (2004), the shrinkage strain is calculated as a positive number.
     """
     return eps_cd + eps_ca
 
@@ -33,7 +38,7 @@ def eps_cs(eps_cd: npt.ArrayLike, eps_ca: npt.ArrayLike) -> npt.ArrayLike:
 def eps_cd(
     beta_ds: npt.ArrayLike, k_h: float, eps_cd_0: float
 ) -> npt.ArrayLike:
-    """Calculates the drying shrinkage.
+    """Calculate the drying shrinkage.
 
     EN 1992-1-1:2004, Eq. (3.9).
 
@@ -55,14 +60,14 @@ def eps_cd(
 
 
 def beta_ds(t: npt.ArrayLike, t_s: float, h_0: float):
-    """Calculates the coefficient taking into account the time of drying.
+    """Calculate the coefficient taking into account the time of drying.
 
     EN 1992-1-1:2004, Eq. (3.10).
 
     Args:
         t (npt.ArrayLike): The age of the concrete in days.
         t_s (float): The age of the concrete in days at the start of drying.
-        h_0 (float): The effective cross section thichkness.
+        h_0 (float): The effective cross section thichkness in mm.
 
     Returns:
         npt.ArrayLike: The coefficient taking into account the time of drying.
@@ -99,7 +104,7 @@ def eps_cd_0(
     beta_RH: float,
     fcm_0: float = 10,
 ) -> float:
-    """Calculates the nominal value of drying shrinkage.
+    """Calculate the nominal value of drying shrinkage.
 
     EN 1992-1-1:2004, Eq. (B.11).
 
@@ -127,7 +132,7 @@ def eps_cd_0(
 
 
 def alpha_ds1(cement_class: t.Literal['S', 'N', 'R']) -> float:
-    """A coefficient depending on the cement class.
+    """Return a coefficient depending on the cement class.
 
     EN 1992-1-1:2004, Sec. B.2.
 
@@ -145,7 +150,7 @@ def alpha_ds1(cement_class: t.Literal['S', 'N', 'R']) -> float:
 
 
 def alpha_ds2(cement_class: t.Literal['S', 'N', 'R']) -> float:
-    """A coefficient depending on the cement class.
+    """Return a coefficient depending on the cement class.
 
     EN 1992-1-1:2004, Sec. B.2.
 
@@ -163,9 +168,7 @@ def alpha_ds2(cement_class: t.Literal['S', 'N', 'R']) -> float:
 
 
 def _get_alpha_ds_dict(cement_class: str) -> t.Dict:
-    """Internal function for getting a dictionary with values for aplha_ds1 and
-    alpha_ds2.
-    """
+    """Return a dictionary with values for aplha_ds1 and alpha_ds2."""
     _alpha_ds = ALPHA_DS_DICT.get(cement_class.upper().strip())
 
     if _alpha_ds is None:
@@ -179,7 +182,7 @@ def _get_alpha_ds_dict(cement_class: str) -> t.Dict:
 
 
 def beta_RH(RH: float, RH_0: float = 100) -> float:
-    """Calculates the factor describing the effect of relative humidity.
+    """Calculate the factor describing the effect of relative humidity.
 
     EN 1992-1-1:2004, Eq. (B.12).
 
@@ -196,7 +199,7 @@ def beta_RH(RH: float, RH_0: float = 100) -> float:
 
 
 def eps_ca(beta_as: npt.ArrayLike, eps_ca_inf: float) -> npt.ArrayLike:
-    """Calculates the autogenous shrinkage.
+    """Calculate the autogenous shrinkage.
 
     EN 1992-1-1:2004, Eq. (3.11).
 
@@ -215,7 +218,7 @@ def eps_ca(beta_as: npt.ArrayLike, eps_ca_inf: float) -> npt.ArrayLike:
 
 
 def eps_ca_inf(fck: float) -> float:
-    """Calculates the final autogenous shrinkage.
+    """Calculate the final autogenous shrinkage.
 
     EN 1992-1-1:2004, Eq. 3.12.
 
@@ -229,7 +232,7 @@ def eps_ca_inf(fck: float) -> float:
 
 
 def beta_as(t: npt.ArrayLike) -> npt.ArrayLike:
-    """Calculates the factor describing the development of autogenous
+    """Calculate the factor describing the development of autogenous
     shrinkage.
 
     EN 1992-1-1:2004, Eq. (3.13).
@@ -281,13 +284,14 @@ def phi_0(phi_RH: float, beta_fcm: float, beta_t0: float) -> float:
 def phi_RH(
     h_0: float, fcm: float, RH: float, alpha_1: float, alpha_2: float
 ) -> float:
-    """Calculates the effect of relative humidity on the standardized creep
+    """Calculate the effect of relative humidity on the standardized creep
     number.
 
     EN 1992-1-1:2004, Eq. (B.3).
 
     Args:
-        h_0 (float): The effective cross sectional thickness, Equation (B.6).
+        h_0 (float): The effective cross sectional thickness in mm, Equation
+            (B.6).
         fcm (float): The mean concrete strength in MPa.
         RH (float): The relative humidity in percent.
         alpha_1 (float): A factor describing the effect of concrete strength
@@ -304,7 +308,7 @@ def phi_RH(
 
 
 def beta_fcm(fcm: float) -> float:
-    """Calculates the effect of the concrete strength on the standardized creep
+    """Calculate the effect of the concrete strength on the standardized creep
     number.
 
     EN 1992-1-1:2004, Eq. (B.4).
@@ -319,7 +323,7 @@ def beta_fcm(fcm: float) -> float:
 
 
 def beta_t0(t0: float) -> float:
-    """Calculates the effect of age at loading on the standardized creep
+    """Calculate the effect of age at loading on the standardized creep
     number.
 
     EN 1992-1-1:2004, Eq. (B.5).
@@ -334,7 +338,7 @@ def beta_t0(t0: float) -> float:
 
 
 def h_0(Ac: float, u: float) -> float:
-    """Calculates the effective thickness of the cross section.
+    """Calculate the effective thickness of the cross section.
 
     EN 1992-1-1:2004, Eq. (B.6).
 
@@ -345,19 +349,23 @@ def h_0(Ac: float, u: float) -> float:
 
     Returns:
         float: The effective thickness.
+
+    Note:
+        The unit of the return will be consistent with the input. E.g. if Ac is
+        mm ** 2 and u is mm, the return is mm.
     """
     return 2 * Ac / u
 
 
 def beta_c(t0: float, t: npt.ArrayLike, beta_H: float) -> float:
-    """Calculates the factor that describes the creep development as a function
+    """Calculate the factor that describes the creep development as a function
     of time after loading.
 
     EN 1992-1-1:2004, Eq. (B.7).
 
     Args:
         t0 (float): The concrete age in days a the time of loading.
-        t (ArrayLike): The concrete age at the evaluated time.
+        t (ArrayLike): The concrete age in days at the evaluated time.
         beta_H (float): Parameter defined in (B.8).
 
     Returns:
@@ -369,13 +377,14 @@ def beta_c(t0: float, t: npt.ArrayLike, beta_H: float) -> float:
 
 
 def beta_H(h_0: float, fcm: float, RH: float, alpha_3) -> float:
-    """Calculates the effect of relative humidity and the effective thickness
+    """Calculate the effect of relative humidity and the effective thickness
     of the structural element.
 
     EN 1992-1-1:2004, Eq. (B.8a and b).
 
     Args:
-        h_0 (float): The effective cross sectional thickness, Equation (B.6).
+        h_0 (float): The effective cross sectional thickness in mm, Equation
+            (B.6).
         fcm (float): The mean concrete strength in MPa.
         RH (float): The relative humidity in percent.
         alpha_3 (float): A factor describing the effect of concrete strength
@@ -396,7 +405,7 @@ def beta_H(h_0: float, fcm: float, RH: float, alpha_3) -> float:
 
 
 def alpha_1(fcm: float) -> float:
-    """A factor taking into account the effect of concrete strength.
+    """Return a factor taking into account the effect of concrete strength.
 
     EN 1992-1-1:2004, Eq. (B.8c).
 
@@ -407,7 +416,7 @@ def alpha_1(fcm: float) -> float:
 
 
 def alpha_2(fcm: float) -> float:
-    """A factor taking into account the effect of concrete strength.
+    """Return a factor taking into account the effect of concrete strength.
 
     EN 1992-1-1:2004, Eq. (B.8c).
 
@@ -418,7 +427,7 @@ def alpha_2(fcm: float) -> float:
 
 
 def alpha_3(fcm: float) -> float:
-    """A factor taking into account the effect of concrete strength.
+    """Return a factor taking into account the effect of concrete strength.
 
     EN 1992-1-1:2004, Eq. (B.8c).
 
@@ -429,7 +438,7 @@ def alpha_3(fcm: float) -> float:
 
 
 def t0_adj(t0: float, alpha_cement: float) -> float:
-    """Calculates the adjusted age of the concrete.
+    """Calculate the adjusted age of the concrete.
 
     EN 1992-1-1:2004, Eq. (B.9).
 
@@ -444,7 +453,7 @@ def t0_adj(t0: float, alpha_cement: float) -> float:
 
 
 def t_T(T: npt.ArrayLike, dt: npt.ArrayLike) -> float:
-    """Calculates the maturity of the concrete.
+    """Calculate the maturity of the concrete.
 
     EN 1992-1-1:2004, Eq. (B.10).
 
@@ -456,7 +465,9 @@ def t_T(T: npt.ArrayLike, dt: npt.ArrayLike) -> float:
         float: The maturity of the concrete.
 
     Note:
-        The two arrays T and dt should have the same length.
+        The two arrays T and dt should have the same length. Each item in dt
+        represents a time interval, and the corresponding item in T represents
+        the average temperature in that time interval.
     """
     # Prepare the input
     T = np.atleast_1d(T)
@@ -473,7 +484,7 @@ def t_T(T: npt.ArrayLike, dt: npt.ArrayLike) -> float:
 
 
 def alpha_cement(cement_class: t.Literal['S', 'N', 'R']) -> float:
-    """An exponent that depends on the cement type.
+    """Return an exponent that depends on the cement type.
 
     Args:
         cement_class (str): The cement class, either 'S', 'N' or 'R'.
