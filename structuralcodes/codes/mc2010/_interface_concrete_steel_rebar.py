ecf797de6ebd052308f9252840d48e9bdb1456a9
Ricky Tai <ricky.tai@tno.nl>
2024-11-22 11:15:02 +0100

    Introduced class Split the method into smaller units

--- structuralcodes/codes/mc2010/_interface_concrete_steel_rebar.py
+++ structuralcodes/codes/mc2010/_interface_concrete_steel_rebar.py
@@ -1,133 +1,153 @@
 """
-A collection of functions for determining the parameters of the analytical bond stress-slip relation, shown in Figure
-6.1-1 of the fib ModelCode 2010.
+Provides functions to determine parameters of the bond stress-slip relations as shown in Figure 6.1-1 of the fib
+ModelCode 2010.
+
+The BondStressSlip class includes methods to calculate:
+- Maximum bond stress (tau_bmax) in case of pull-out failure.
+- Bond stress at reinforcement yield (tau_yield).
+- Bond stress at splitting failure (tau_bu_split).
+- Slip corresponding to bond stress at splitting failure (s_tau_bu_split).
 """
 
 import warnings
 
 
-def tau_bu_split(
-        f_cm: float,
-        phi: float,
-        c_min: float,
-        c_max: float,
-        k_m: float,
-        K_tr: float,
-        bond: str,
-        f_ym: float = None,
-) -> dict:
-    """
-    Eq. (6.1-5) of MC2010. This equation is derived from Eq. (6.1-19). Unlike in the ModelCode, the limit values and
-    input validity of Eq. (6.1-19) are also included in this function.
+class BondStressSlip:
+    def __init__(self, f_cm: float, bond: str):
+        """
+        Initialize the BondStressSlip class with concrete strength and bond condition.
 
-    Args:
-        bond: "Good" or "Poor" bond conditions, described in section 6.1.3.2 of the fib ModelCode 2010;
-        f_cm: Mean value of compressive strength of concrete in MPa;
-        phi: Nominal bar diameter in mm;
-        c_min: Figure 6.1-2 of the fib ModelCode 2010;
-        c_max: Figure 6.1-2 of the fib ModelCode 2010;
-        k_m: Figure 6.1-3 of the fib ModelCode 2010;
-        K_tr: Eq (6.1-6) of the fib ModelCode 2010;
-        f_ym: Mean yield stress of the reinforcement in MPa;
+        Args:
+            f_cm: Mean concrete cylindrical compressive strength in MPa.
+            bond: "Good" or "Other" bond conditions, according to MC2010 section 6.1.3.2.
+        """
 
-    Returns:
-        Dictionary with ultimate bond stress corresponding to splitting, yield and pull-out, and the failure mode and
-        failure bond stress.
-    """
+        if bond not in ["Good", "Other"]:
+            raise ValueError("Invalid input for bond. Use 'Good' or 'Other'")
 
-    # Raise warnings
-    if not 15 < f_cm < 110:
-        warnings.warn("Warning: Eq. (6.1-19) is valid for 15 MPa < f_cm < 110 MPa")
-    if not 0.5 < c_min / phi < 3.5:
-        warnings.warn("Warning: Eq. (6.1-19) is valid for 0.5 < c_min / phi < 3.5")
-    if not 1.0 < c_max / c_min < 5.0:
-        warnings.warn("Warning: Eq. (6.1-19) is valid for 1.0 < c_max / c_min < 5.0")
-    if not K_tr <= 0.05:
-        warnings.warn("Warning: Eq. (6.1-19) is valid for K_tr <= 0.05")
-    if f_ym is None:
-        warnings.warn(
-            "Warning: No input for f_ym.")
+        self.f_cm = f_cm
+        self.bond = bond
+        self._tau_bmax = None # Initialize as None
+        self._tau_bu_split = None  # Initialize as None
+        self._s_tau_bu_split = None  # Initialize as None
 
+    def tau_bmax(self) -> float:
+        """
+        Calculate maximum bond stress in case of pull-out failure, according to MC2010 Table 6.1-1
 
-    bond_param = {
-        'Good': {
-            'tau_bmax': 2.5 * (f_cm) ** 0.5,
-            'eta_2': 1.0
-        },
-        'Poor': {
-            'tau_bmax': 1.25 * (f_cm) ** 0.5,
-            'eta_2': 0.7
-        }
-    }
+        Returns:
+            tau_bmax in MPa.
+        """
+        if self._tau_bmax is None:  # Calculate only if not already calculated
+            if self.bond == 'Good':
+                self._tau_bmax = 2.5 * self.f_cm ** 0.5
+            if self.bond == 'Other':
+                self._tau_bmax = 1.25 * self.f_cm ** 0.5
+        return self._tau_bmax
 
-    tau_bu_split = (
-            bond_param[bond]['eta_2']
-            * 6.5
-            * (f_cm / 25) ** 0.25
-            * (25 / phi) ** 0.2
-            * (
-                    (c_min / phi) ** 0.25  # <-- Deviates from MC2010, but follows fib Bulletin 72, Appendix A.
-                    * (c_max / c_min) ** 0.1
-                    + k_m * K_tr
+    def tau_bu_split(
+            self,
+            phi: float,
+            c_min: float,
+            c_max: float,
+            k_m: float,
+            K_tr: float,
+    ) -> float:
+        """
+        Eq.(6.1-5) of MC2010.
+        Raises warnings if parameter input values are invalid for MC2010 Eq.(6.1-19), and consequently for Eq.(6.1-5).
+
+        Args:
+            phi: Nominal bar diameter in mm;
+            c_min: Parameter according to MC2010 Figure 6.1-2;
+            c_max: Parameter according to MC2010Figure 6.1-2;
+            k_m: Parameter according to MC2010Figure 6.1-3;
+            K_tr: To be calculated with MC2010 Eq.(6.1-6);
+
+        Returns:
+            tau_bu,split in MPa.
+        """
+        if self._tau_bu_split is None:  # Calculate only if not already calculated
+            # Raise warnings
+            if not 15 < self.f_cm < 110:
+                warnings.warn("Warning: Eq.(6.1-19) is valid for 15 MPa < f_cm < 110 MPa")
+            if not 0.5 < c_min / phi < 3.5:
+                warnings.warn("Warning: Eq.(6.1-19) is valid for 0.5 < c_min / phi < 3.5")
+            if not 1.0 < c_max / c_min < 5.0:
+                warnings.warn("Warning: Eq.(6.1-19) is valid for 1.0 < c_max / c_min < 5.0")
+            if not K_tr <= 0.05:
+                warnings.warn("Warning: Eq.(6.1-19) is valid for K_tr <= 0.05")
+
+            if self.bond == 'Good':
+                eta_2 = 1.0
+            if self.bond == 'Other':
+                eta_2 = 0.7
+
+            self._tau_bu_split = (
+                    eta_2
+                    * 6.5
+                    * (self.f_cm / 25) ** 0.25
+                    * (25 / phi) ** 0.2
+                    * (
+                            (c_min / phi) ** 0.25  # <-- Differs from MC2010, follows fib Bulletin 72, Appendix A.
+                            * (c_max / c_min) ** 0.1
+                            + k_m * K_tr
+                    )
             )
-    )
 
-    tau_all = {
-        'Pull-out': bond_param[bond]['tau_bmax'],
-        'Splitting': tau_bu_split,
-    }
+            return self._tau_bu_split
 
-    if f_ym is None:
-        msg = 'Potential yielding of rebar prior to reaching tau_bu_split or tau_bmax was not checked.'
-        if len(tau_all) != len(set(tau_all)):
-            warnings.warn("Warning: The tau of two or more failure modes is identical")
-        else:
-           tau_fail = min(tau_all.values())
-           fail_mode = f'{min(tau_all, key=tau_all.get)}. Warning: {msg}'
-    else:
-        tau_all['Yield'] = f_ym / 20,  # Based on the assumption that the bond stress is uniform over l_b = 5 * phi
-        if len(tau_all) != len(set(tau_all)):
-            warnings.warn("Warning: The tau of two or more failure modes is identical")
-        else:
-           tau_fail = min(tau_all.values())
-           fail_mode = min(tau_all, key=tau_all.get)
+    def s_tau_bu_split(
+            self,
+            tau_bu_split: float,
+    ) -> float:
+        """
+        Calculate the slip, s, corresponding to tau_bu,split according to MC2010 Eq.(6.1-1).
 
-    return {
-        'tau_bu_split': tau_all['Splitting'],
-        'tau_yield': tau_all['Yield'],
-        'tau_bmax': tau_all['Pull-out'],
-        'fail_mode': fail_mode,
-        'tau_fail': tau_fail,
-    }
+        Args:
+            tau_bu_split: Concrete bond stress at splitting failure.
 
+        Returns:
+            The slip corresponding to tau_bu,split.
+        """
+        if self._s_tau_bu_split is None:  # Calculate only if not already calculated
+            if self.bond == 'Good':
+                s1_PO = 1.0
+            elif self.bond == 'Other':
+                s1_PO = 1.8
 
-def s_tau_bu_split(
-        f_cm: float,
-        bond: str,
-        tau_bu_split: float,
-) -> float:
-    """
-    Calculate s(tau_bu,split) of ModelCode 2010 Table 6.1-1.
+            tau_bmax = self.tau_bmax()
+            self._s_tau_bu_split = s1_PO * (tau_bu_split / tau_bmax) ** (1 / 0.4)
 
+        return self._s_tau_bu_split
 
-    Args:
-        f_cm: Mean value of compressive strength of concrete in MPa;
-        bond: "Good" or "Poor" bond conditions, as defined in fib ModelCode 2010 section 6.1.3.2;
-        tau_bu_split: Concrete bond stress at splitting failure.
+    @staticmethod
+    def tau_yield(
+            f_ym: float,
+            l_b: float = None,
+            phi: float = None,
+    ) -> float:
+        """
+        Bond stress at reinforcement yield.
 
-    Returns:
-        s(tau_bu,split)
-    """
+        Both l_b and phi must be provided together or both must have no input. In the latter case a uniform bond stress
+        over 5 times the bar diameter is assumed, according to MC2010 Eq.(6.1-5)
 
-    alpha = 0.4
+        Args:
+            f_ym: Reinforcement mean yield stress.
+            l_b: Assumed length with uniform bond stress.
+            phi: Reinforcement diameter.
 
-    if bond == 'Good':
-        s1_PO = 1.0
-        tau_bmax = 2.5 * f_cm ** 0.5
-    elif bond == 'Poor':
-        s1_PO = 2.0
-        tau_bmax = 1.25 * f_cm ** 0.5
-    else:
-        warnings.ValueError("Invalid bond condition. Use 'Good' or 'Poor'")
+        Returns:
+            Bond stress at reinforcement yield.
+        """
+        if (l_b is None and phi is not None) or (l_b is not None and phi is None):
+            raise ValueError("Both l_b and phi must be provided together or both must be excluded.")
 
-    return s1_PO * (tau_bu_split / tau_bmax) ** (1 / alpha)
+        # If both are excluded, assume uniform bond stress over l_b = 5 * phi
+        if l_b is None and phi is None:
+            l_b = 5
+            phi = 1
+
+        # tau_yield derived from the equilibrium: f_ym * 0.25 * pi * phi ** 2 = pi * phi * l_b * tau_yield
+        return f_ym * phi / 4 / l_b
