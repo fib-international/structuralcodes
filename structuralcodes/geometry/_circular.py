af612454d96a981b9e6c3b4ff744a843d815324b
talledodiego <38036285+talledodiego@users.noreply.github.com>
2024-11-22 21:45:26 +0100

    attempt circularRC

--- structuralcodes/geometry/_circular.py
+++ structuralcodes/geometry/_circular.py
@@ -13,7 +13,7 @@ from shapely import Polygon
 
 from structuralcodes.core.base import ConstitutiveLaw, Material
 
-from ._geometry import SurfaceGeometry
+from ._geometry import CompoundGeometry, SurfaceGeometry
 
 
 def _create_circle(radius, npoints=20):
@@ -41,7 +41,7 @@ class CircularGeometry(SurfaceGeometry):
         density: t.Optional[float] = None,
         concrete: bool = False,
     ) -> None:
-        """Initialize a CircularSection.
+        """Initialize a CircularGeometry.
 
         Arguments:
             size (float): The size of the geometry. By default this number
@@ -49,8 +49,8 @@ class CircularGeometry(SurfaceGeometry):
                 as the radius `is_radius` should be set as `True`.
             material (Union(Material, ConstitutiveLaw)): A Material or
                 ConsitutiveLaw class applied to the geometry.
-                is_radius (bool): Indicates if size is interpreted as radius
-                    (default = False).
+            is_radius (bool): Indicates if size is interpreted as radius
+                (default = False).
             n_points (int): The number of points used to discretize the
                 circle as a shapely `Polygon` (default = 20).
             density (Optional(float)): When a ConstitutiveLaw is passed as
@@ -70,6 +70,87 @@ class CircularGeometry(SurfaceGeometry):
         self._radius = size if is_radius else size / 2.0
         # Create the shapely polygon
         polygon = _create_circle(radius=self._radius, npoints=n_points)
+        # Pass everything to the base class
+        super().__init__(
+            poly=polygon, material=material, density=density, concrete=concrete
+        )
+
+    @property
+    def radius(self):
+        """Returns the radius of the circle."""
+        return self._radius
+
+    @property
+    def diameter(self):
+        """Return diameter of the circle."""
+        return 2 * self._radius
+
+
+class CircularRCGeometry(CompoundGeometry):
+    """This is a wrapper class for defining a CompoundGeometry made of a
+    surface with circular shape and with a set of bars around the circle.
+    """
+
+    _radius: float
+
+    def __init__(
+        self,
+        size: float,
+        surface_material: t.Union[Material, ConstitutiveLaw],
+        diameter: float,
+        number: int,
+        reinforcement_material: t.Union[Material, ConstitutiveLaw],
+        is_radius: bool = False,
+        n_points: int = 20,
+        surface_density: t.Optional[float] = None,
+        reinforcement_density: t.Optional[float] = None,
+        concrete: bool = False,
+    ) -> None:
+        """Initialize a CircularRCGeometry.
+
+        Arguments:
+            size (float): The size of the geometry. By default this number
+                is assumed as the diameter of the section. To interpret this
+                as the radius `is_radius` should be set as `True`.
+            suface_material (Union(Material, ConstitutiveLaw)): A Material or
+                ConsitutiveLaw class applied to the surface geometry.
+            diameter (float): The diameter of the bars.
+            number (int): The number of bars to distribute.
+            reinforcement_material (Union(Material, ConstitutiveLaw)): A
+                Material or ConsitutiveLaw class applied to the point geometry.
+            is_radius (bool): Indicates if size is interpreted as radius
+                    (default = False).
+            n_points (int): The number of points used to discretize the
+                circle as a shapely `Polygon` (default = 20).
+            surface_density (Optional(float)): When a ConstitutiveLaw is passed
+                as surface_material, the density can be provided by this
+                argument. When surface_material is a Material object the
+                density is taken from the material.
+            reinforcement_density (Optional(float)): When a ConstitutiveLaw is
+                passed as reinforcement_material, the density can be provided
+                by this argument. When reinforcement_material is a Material
+                object the density is taken from the material.
+            concrete (bool): Flag to indicate if the surface geometry is
+                concrete.
+
+        Note:
+            The CircularRCGeometry is simply a wrapper for a CompoundGeometry
+            object.
+        """
+        # Check that size is strictly positive
+        if size <= 0:
+            raise ValueError('Size must be a positive number.')
+        # Manage size as radius or diameter (default)
+        self._radius = size if is_radius else size / 2.0
+        # Create the CircularGeometry
+        geometry = CircularGeometry(
+            size,
+            surface_material,
+            is_radius,
+            n_points,
+            surface_density,
+            concrete,
+        )
         # Pass everything to the base class
         super().__init__(
             poly=polygon, material=material, density=density, concrete=concrete
