daa9be9434352efb0e4572d599a60cb1194f04bf
talledodiego <38036285+talledodiego@users.noreply.github.com>
2024-11-28 12:24:35 +0100

    refactor fiber integrator
    Factoring out triangulate method from prepare_input

--- structuralcodes/sections/section_integrators/_fiber_integrator.py
+++ structuralcodes/sections/section_integrators/_fiber_integrator.py
@@ -9,6 +9,7 @@ import triangle
 from numpy.typing import ArrayLike
 from shapely import Polygon
 
+from structuralcodes.core.base import ConstitutiveLaw
 from structuralcodes.geometry import CompoundGeometry, SurfaceGeometry
 
 from ._section_integrator import SectionIntegrator
@@ -18,7 +19,7 @@ class FiberIntegrator(SectionIntegrator):
     """Section integrator based on the Marin algorithm."""
 
     def prepare_triangulation(self, geo: SurfaceGeometry) -> t.Dict:
-        """Triangulate a SurfaceGeometry object.
+        """Prepare data for triangulating it with triangle.
 
         Arguments:
             geo (SurfaceGeometry): The geometry to triangulate.
@@ -60,6 +61,100 @@ class FiberIntegrator(SectionIntegrator):
             tri['holes'] = holes
         return tri
 
+    def triangulate(
+        self, geo: CompoundGeometry, mesh_size: float
+    ) -> t.List[t.Tuple[np.ndarray, np.ndarray, np.ndarray, ConstitutiveLaw]]:
+        """Triangulate the geometry discretizing it into fibers.
+
+        Arguments:
+            geo (CompoundGeometry): The geometry of the section.
+            mesh_size (float): Percentage of area (number from 0 to 1) max for
+                triangle elements.
+
+        Raises:
+            ValueError if mesh_size is <= 0.0 or if mesh_size is > 1.0.
+        """
+        # Check mesh_size being between 0 and 1.
+        if mesh_size <= 0 or mesh_size > 1:
+            raise ValueError('mesh_size is a number from 0 to 1')
+
+        triangulated_data = []
+
+        # For the surface geometries
+        for g in geo.geometries:
+            # prepare data structure for triangle module
+            tri = self.prepare_triangulation(g)
+            # define the maximum area of the triangles
+            max_area = g.area * mesh_size
+            # triangulate the geometry getting back the mesh
+            mesh = triangle.triangulate(tri, f'pq{30:.1f}Aa{max_area}o1')
+            mat = g.material
+            # Get x and y coordinates (centroid) and area for each fiber
+            x = []
+            y = []
+            area = []
+            for tr in mesh['triangles']:
+                # get centroid of triangle
+                xc = (
+                    mesh['vertices'][tr[0]][0]
+                    + mesh['vertices'][tr[1]][0]
+                    + mesh['vertices'][tr[2]][0]
+                )
+                xc /= 3.0
+                x.append(xc)
+                yc = (
+                    mesh['vertices'][tr[0]][1]
+                    + mesh['vertices'][tr[1]][1]
+                    + mesh['vertices'][tr[2]][1]
+                )
+                yc /= 3.0
+                y.append(yc)
+                # compute area
+                a = (
+                    mesh['vertices'][tr[0]][0] * mesh['vertices'][tr[1]][1]
+                    - mesh['vertices'][tr[0]][1] * mesh['vertices'][tr[1]][0]
+                )
+                a += (
+                    mesh['vertices'][tr[1]][0] * mesh['vertices'][tr[2]][1]
+                    - mesh['vertices'][tr[1]][1] * mesh['vertices'][tr[2]][0]
+                )
+                a += (
+                    mesh['vertices'][tr[2]][0] * mesh['vertices'][tr[0]][1]
+                    - mesh['vertices'][tr[2]][1] * mesh['vertices'][tr[0]][0]
+                )
+                a = abs(a) * 0.5
+                area.append(a)
+                # pointer to the material
+
+            # return back the triangulation data
+            triangulated_data.append(
+                (np.array(x), np.array(y), np.array(area), mat)
+            )
+        # For the reinforcement
+        # Tentative proposal for managing reinforcement (PointGeometry)
+        reinf_data = {}
+        # Preprocess geometries having the same material
+        for pg in geo.point_geometries:
+            x, y = pg._point.coords.xy
+            x = x[0]
+            y = y[0]
+            area = pg.area
+            mat = pg.material
+            if reinf_data.get(mat) is None:
+                reinf_data[mat] = [
+                    np.array(x),
+                    np.array(y),
+                    np.array(area),
+                ]
+            else:
+                reinf_data[mat][0] = np.hstack((reinf_data[mat][0], x))
+                reinf_data[mat][1] = np.hstack((reinf_data[mat][1], y))
+                reinf_data[mat][2] = np.hstack((reinf_data[mat][2], area))
+        for mat, value in reinf_data.items():
+            triangulated_data.append((value[0], value[1], value[2], mat))
+
+        return triangulated_data
+
     def prepare_input(
         self, geo: CompoundGeometry, strain: ArrayLike, **kwargs
     ) -> t.Tuple[t.Tuple[np.ndarray, np.ndarray, np.ndarray]]:
@@ -67,18 +162,20 @@ class FiberIntegrator(SectionIntegrator):
 
         Calculate the stresses based on strains in a set of points.
 
-        Keyword Arguments:
+        Arguments:
             geo (CompoundGeometry): The geometry of the section.
             strain (ArrayLike): The strains and curvatures of the section,
                 given in the format (ea, ky, kz) which are i) strain at 0,0,
                 ii) curvature y axis, iii) curvature z axis.
-            mesh_size: Percentage of area (number from 0 to 1) max for triangle
-                elements.
+
+        Keyword Arguments:
+            mesh_size (float): Percentage of area (number from 0 to 1) max for
+                triangle elements.
 
         Returns:
-            Tuple(List, Dict): The prepared input representing a list with
+            Tuple(List): The prepared input representing a list with
             x-coordinates, y-coordinates and force for each fiber and a
-            dictionary containing the triangulation data that can be stored and
+            list containing the triangulation data that can be stored and
             used later to avoid repetition of triangulation.
         """
         # This method should:
@@ -92,85 +189,9 @@ class FiberIntegrator(SectionIntegrator):
             # No triangulation is provided, triangulate the section
             # Fiber integrator for generic section uses delaunay triangulation
             # for discretizing in fibers
-            triangulated_data = []
-            mesh_size = kwargs.get('mesh_size', 0.01)
-            if mesh_size <= 0 or mesh_size > 1:
-                raise ValueError('mesh_size is a number from 0 to 1')
-            # For the surface geometries
-            for g in geo.geometries:
-                # prepare data structure for triangle module
-                tri = self.prepare_triangulation(g)
-                # define the maximum area of the triangles
-                max_area = g.area * mesh_size
-                # triangulate the geometry getting back the mesh
-                mesh = triangle.triangulate(tri, f'pq{30:.1f}Aa{max_area}o1')
-                mat = g.material
-                # Get x and y coordinates (centroid) and area for each fiber
-                x = []
-                y = []
-                area = []
-                for tr in mesh['triangles']:
-                    # get centroid of triangle
-                    xc = (
-                        mesh['vertices'][tr[0]][0]
-                        + mesh['vertices'][tr[1]][0]
-                        + mesh['vertices'][tr[2]][0]
-                    )
-                    xc /= 3.0
-                    x.append(xc)
-                    yc = (
-                        mesh['vertices'][tr[0]][1]
-                        + mesh['vertices'][tr[1]][1]
-                        + mesh['vertices'][tr[2]][1]
-                    )
-                    yc /= 3.0
-                    y.append(yc)
-                    # compute area
-                    a = (
-                        mesh['vertices'][tr[0]][0] * mesh['vertices'][tr[1]][1]
-                        - mesh['vertices'][tr[0]][1]
-                        * mesh['vertices'][tr[1]][0]
-                    )
-                    a += (
-                        mesh['vertices'][tr[1]][0] * mesh['vertices'][tr[2]][1]
-                        - mesh['vertices'][tr[1]][1]
-                        * mesh['vertices'][tr[2]][0]
-                    )
-                    a += (
-                        mesh['vertices'][tr[2]][0] * mesh['vertices'][tr[0]][1]
-                        - mesh['vertices'][tr[2]][1]
-                        * mesh['vertices'][tr[0]][0]
-                    )
-                    a = abs(a) * 0.5
-                    area.append(a)
-                    # pointer to the material
 
-                # return back the triangulation data
-                triangulated_data.append(
-                    (np.array(x), np.array(y), np.array(area), mat)
-                )
-            # For the reinforcement
-            # Tentative proposal for managing reinforcement (PointGeometry)
-            reinf_data = {}
-            # Preprocess geometries having the same material
-            for pg in geo.point_geometries:
-                x, y = pg._point.coords.xy
-                x = x[0]
-                y = y[0]
-                area = pg.area
-                mat = pg.material
-                if reinf_data.get(mat) is None:
-                    reinf_data[mat] = [
-                        np.array(x),
-                        np.array(y),
-                        np.array(area),
-                    ]
-                else:
-                    reinf_data[mat][0] = np.hstack((reinf_data[mat][0], x))
-                    reinf_data[mat][1] = np.hstack((reinf_data[mat][1], y))
-                    reinf_data[mat][2] = np.hstack((reinf_data[mat][2], area))
-            for mat, value in reinf_data.items():
-                triangulated_data.append((value[0], value[1], value[2], mat))
+            mesh_size = kwargs.get('mesh_size', 0.01)
+            triangulated_data = self.triangulate(geo, mesh_size)
 
         x = []
         y = []
