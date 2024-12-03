321940eb8a1a7fe26e53a454958ab6200cfebf39
talledodiego <38036285+talledodiego@users.noreply.github.com>
2024-12-03 23:22:03 +0100

    Refactored Marin Stress Integrator

--- structuralcodes/sections/section_integrators/_marin_integrator.py
+++ structuralcodes/sections/section_integrators/_marin_integrator.py
@@ -19,6 +19,109 @@ from ._section_integrator import SectionIntegrator
 class MarinIntegrator(SectionIntegrator):
     """Section integrator based on the Marin algorithm."""
 
+    def _rotate_geometry(
+        self, geo: CompoundGeometry, strain: ArrayLike
+    ) -> t.Tuple[float, CompoundGeometry, ArrayLike]:
+        """This function returns the geometry, strain and angle in a rotated
+        CRS for which bending is uniaxial.
+        """
+        # Rotate section in order to have neutral axis horizontal
+        angle = -atan2(strain[2], strain[1])
+
+        rotated_geom = geo.rotate(angle)
+        # Determine strain in this rotated CRS
+        strain_rotated = [strain[0], (strain[2] ** 2 + strain[1] ** 2) ** 0.5]
+
+        return angle, rotated_geom, strain_rotated
+
+    def _get_input_polygon(
+        self, polygon: Polygon, coeffs: ArrayLike, input: list
+    ):
+        """Appends to input list the coordinates and coefficient of polygon."""
+        # Let's be sure to orient in the right way
+        if polygon.is_empty:
+            return
+        polygon = orient(polygon, 1)
+        if not polygon.exterior.is_ccw:
+            raise ValueError(
+                'The exterior of a polygon should have vertices \
+                            ordered ccw'
+            )
+        # Manage exterior part
+        x, y = polygon.exterior.coords.xy
+        x = np.array(x)
+        y = np.array(y)
+        input.append((0, np.array(x), np.array(y), np.array(coeffs)))
+        # Manage holes
+        for i in polygon.interiors:
+            if i.is_ccw:
+                raise ValueError('A inner hole should have cw coordinates')
+            x, y = i.coords.xy
+            input.append((0, np.array(x), np.array(y), np.array(coeffs)))
+
+    def _process_surface_geometries(
+        self, geo: CompoundGeometry, strain: ArrayLike, input: t.List
+    ):
+        """Process Surface geometries filling the input data for each one."""
+        # For each SurfaceGeometry on the CompoundGeometry:
+        for g in geo.geometries:
+            # Get coefficients and strain limits from constitutive law
+            if hasattr(g.material, '__marin__'):
+                strains, coeffs = g.material.__marin__(strain=strain)
+            else:
+                raise AttributeError(
+                    f'The material object {g.material} of geometry {g} does \
+                    not have implement the __marin__ function. \
+                    Please implement the function or use another integrator, \
+                    like '
+                    'Fibre'
+                    ''
+                )
+
+            # Subdivide the polygon at the different strain limits
+            if strains is None:
+                self._get_input_polygon(g.polygon, coeffs[0], input)
+            else:
+                for p in range(len(strains)):
+                    # Create the two lines for selecting the needed part
+                    y0 = -(strain[0] - strains[p][0]) / strain[1]
+                    y1 = -(strain[0] - strains[p][1]) / strain[1]
+                    if y0 > y1:
+                        y0, y1 = y1, y0
+                    bbox = g.polygon.bounds
+                    line0 = create_line_point_angle((0, y0), 0, bbox)
+                    line1 = create_line_point_angle((0, y1), 0, bbox)
+                    lines = MultiLineString((line0, line1))
+                    # intersection
+                    result = g.split_two_lines(lines=lines)
+
+                    if isinstance(result, Polygon):
+                        # If the result is a single polygon
+                        self._get_input_polygon(result, coeffs[p], input)
+                    elif isinstance(result, MultiPolygon):
+                        # If the result is a MultiPolygon
+                        for polygon in result.geoms:
+                            self._get_input_polygon(polygon, coeffs[p], input)
+
+    def _process_point_geometries(
+        self, geo: CompoundGeometry, strain: ArrayLike, input: t.List
+    ):
+        """Process Point geometries filling the input data."""
+        # Tentative proposal for managing reinforcement (PointGeometry)
+        x = []
+        y = []
+        F = []
+        for pg in geo.point_geometries:
+            xp, yp = pg._point.coords.xy
+            xp = xp[0]
+            yp = yp[0]
+            A = pg.area
+            strain_ = strain[0] + strain[1] * yp
+            x.append(xp)
+            y.append(yp)
+            F.append(pg.material.get_stress(strain_) * A)
+        input.append((1, np.array(x), np.array(y), np.array(F)))
+
     def prepare_input_stress(
         self, geo: CompoundGeometry, strain: ArrayLike
     ) -> t.Tuple[float, t.Tuple[np.ndarray, np.ndarray, np.ndarray]]:
@@ -40,111 +143,25 @@ class MarinIntegrator(SectionIntegrator):
             back the resultants) and as a tuple with 3 ndarrys collecting
             respectively y, z and stress coefficients for each sub-part.
         """
-        # This method should do the following tasks:
-        # - For each geo:
-        #   - ask constitutive law strain limits and coefficients
-        #   - For each material part, the part should furthermore be split
-        # according to constant, linear and parabolic stress distribution.
-        #   - For each part collect coordinates y and z in separate np.ndarray
-        # iterables, and stress coefficients in a two-dimensional np.ndarray.
-        #
         # The method should therefore return a tuple that collects the y, z,
         # and stress coefficients for each part.
         prepared_input = []
-        # 1. Rotate section in order to have neutral axis horizontal
-        angle = -atan2(strain[2], strain[1])
+        # Rotate section in order to have neutral axis horizontal
+        angle, rotated_geom, strain_rotated = self._rotate_geometry(
+            geo, strain
+        )
 
-        rotated_geom = geo.rotate(angle)
-        # 2. Get y coordinate of neutral axis in this new CRS
-        strain_rotated = [strain[0], (strain[2] ** 2 + strain[1] ** 2) ** 0.5]
+        # Process all the surface geometries splitting them and appending
+        # coefficients and strain limits to prepared_input
+        self._process_surface_geometries(
+            rotated_geom, strain_rotated, prepared_input
+        )
 
-        # 3. For each SurfaceGeometry on the CompoundGeometry:
-        for g in rotated_geom.geometries:
-            # 3a. get coefficients and strain limits from constitutive law
-            if hasattr(g.material, '__marin__'):
-                strains, coeffs = g.material.__marin__(strain=strain_rotated)
-            else:
-                raise AttributeError(
-                    f'The material object {g.material} of geometry {g} does \
-                    not have implement the __marin__ function. \
-                    Please implement the function or use another integrator, \
-                    like '
-                    'Fibre'
-                    ''
-                )
-
-            # 3b. Subdivide the polygon at the different strain limits
-            def get_input_polygon(polygon, coeffs):
-                # Let's be sure to orient in the right way
-                if polygon.is_empty:
-                    return
-                polygon = orient(polygon, 1)
-                if not polygon.exterior.is_ccw:
-                    raise ValueError(
-                        'The exterior of a polygon should have vertices \
-                                    ordered ccw'
-                    )
-                # Manage exterior part
-                x, y = polygon.exterior.coords.xy
-                x = np.array(x)
-                y = np.array(y)
-                prepared_input.append(
-                    (0, np.array(x), np.array(y), np.array(coeffs))
-                )
-                # Manage holes
-                for i in polygon.interiors:
-                    if i.is_ccw:
-                        raise ValueError(
-                            'A inner hole should have cw coordinates'
-                        )
-                    x, y = i.coords.xy
-                    prepared_input.append(
-                        (0, np.array(x), np.array(y), np.array(coeffs))
-                    )
-
-            if strains is None:
-                get_input_polygon(g.polygon, coeffs[0])
-            else:
-                for p in range(len(strains)):
-                    # Create the two lines for selecting the needed part
-                    y0 = (
-                        -(strain_rotated[0] - strains[p][0])
-                        / strain_rotated[1]
-                    )
-                    y1 = (
-                        -(strain_rotated[0] - strains[p][1])
-                        / strain_rotated[1]
-                    )
-                    if y0 > y1:
-                        y0, y1 = y1, y0
-                    bbox = g.polygon.bounds
-                    line0 = create_line_point_angle((0, y0), 0, bbox)
-                    line1 = create_line_point_angle((0, y1), 0, bbox)
-                    lines = MultiLineString((line0, line1))
-                    # intersection
-                    result = g.split_two_lines(lines=lines)
-
-                    if isinstance(result, Polygon):
-                        # If the result is a single polygon
-                        get_input_polygon(result, coeffs[p])
-                    elif isinstance(result, MultiPolygon):
-                        # If the result is a MultiPolygon
-                        for polygon in result.geoms:
-                            get_input_polygon(polygon, coeffs[p])
-        # Tentative proposal for managing reinforcement (PointGeometry)
-        x = []
-        y = []
-        F = []
-        for pg in rotated_geom.point_geometries:
-            xp, yp = pg._point.coords.xy
-            xp = xp[0]
-            yp = yp[0]
-            A = pg.area
-            strain = strain_rotated[0] + strain_rotated[1] * yp
-            x.append(xp)
-            y.append(yp)
-            F.append(pg.material.get_stress(strain) * A)
-        prepared_input.append((1, np.array(x), np.array(y), np.array(F)))
+        # Process all the point geometries (i.e. reinforcement) splitting them
+        # and appending coefficients and strain limits to prepared_input
+        self._process_point_geometries(
+            rotated_geom, strain_rotated, prepared_input
+        )
 
         return angle, prepared_input
 
