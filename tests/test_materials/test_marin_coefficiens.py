2367d7cefa66bc1576265d5fa19d0b51c34978db
talledodiego <38036285+talledodiego@users.noreply.github.com>
2024-12-13 09:26:07 +0100

    add tests marin coefficients parabola rectangle

--- tests/test_materials/test_marin_coefficiens.py
+++ tests/test_materials/test_marin_coefficiens.py
@@ -9,6 +9,7 @@ from structuralcodes.materials.constitutive_laws import (
     BilinearCompression,
     Elastic,
     ElasticPlastic,
+    ParabolaRectangle,
 )
 
 eps0 = (
@@ -350,6 +351,133 @@ def test_marin_elasticplastic_axial_bending_strain(
             (Eh,),
             (E,),
             (Eh,),
+        ],
+    )
+
+    strain, coeffs = law.__marin_tangent__([eps_0, kappa_y])
+
+    assert len(strain) == len(expected[0])
+    assert len(expected[1]) == len(coeffs)
+
+    for coeff, expect in zip(coeffs, expected[1]):
+        assert len(coeff) == len(expect)
+        for i in range(len(coeff)):
+            assert math.isclose(coeff[i], expect[i])
+
+
+@pytest.mark.parametrize('eps_0', eps0)
+@pytest.mark.parametrize(
+    'fc, eps_c0, eps_cu',
+    [
+        (-30, -0.002, -0.0035),
+        (-20, -0.0015, -0.004),
+        (-35, -0.002, -0.003),
+        (-20000, -0.002, -0.0025),
+        (-30000, -0.0025, -0.0035),
+    ],
+)
+def test_marin_parabolarectangle_uniform_strain(eps_0, fc, eps_c0, eps_cu):
+    """Test the marin coefficients for a strain plane of pure tension or
+    compression for ParabolaRectangle constitutive law.
+    """
+    law = ParabolaRectangle(fc, eps_c0, eps_cu)
+
+    # Check marin coefficients for stress integration
+    if eps_0 > 0:
+        expected = (None, (0.0,))
+    elif eps_0 > eps_c0:
+        expected = (
+            None,
+            (fc / eps_c0 * eps_0 * (2 - eps_0 / eps_c0), 0.0, 0.0),
+        )
+    elif eps_0 >= eps_cu:
+        expected = (None, (fc,))
+    else:
+        expected = (None, (0.0,))
+
+    strain, coeffs = law.__marin__([eps_0, 0])
+
+    assert strain == expected[0]
+    assert len(coeffs[0]) == len(expected[1])
+    for i in range(len(coeffs[0])):
+        assert math.isclose(coeffs[0][i], expected[1][i])
+
+    # Check marin coefficients for modulus integration
+    if eps_0 > 0:
+        expected = (None, (0.0,))
+    elif eps_0 > eps_c0:
+        expected = (None, (2 * fc / eps_c0 * (1 - eps_0 / eps_c0), 0.0))
+    else:
+        expected = (None, (0.0,))
+
+    strain, coeffs = law.__marin_tangent__([eps_0, 0])
+
+    assert strain == expected[0]
+    assert len(coeffs[0]) == len(expected[1])
+    for i in range(len(coeffs[0])):
+        assert math.isclose(coeffs[0][i], expected[1][i])
+
+
+@pytest.mark.parametrize('eps_0', eps0)
+@pytest.mark.parametrize(
+    'eps_min', [(-0.01), (-0.005), (-0.004), (-0.003), (-0.002), (-0.001)]
+)
+@pytest.mark.parametrize(
+    'fc, eps_c0, eps_cu, h',
+    [
+        (-30, -0.002, -0.0035, 400),
+        (-20, -0.0015, -0.004, 400),
+        (-35, -0.002, -0.003, 350),
+        (-20000, -0.002, -0.0025, 0.4),
+        (-30000, -0.0025, -0.0035, 0.3),
+    ],
+)
+def test_marin_parabolarectangle_axial_bending_strain(
+    eps_0, eps_min, fc, eps_c0, eps_cu, h
+):
+    """Test the marin coefficients for a strain plane of uniaxial bending
+    with tension / compression for ParabolaRectangle constitutive law.
+    """
+    law = ParabolaRectangle(fc, eps_c0, eps_cu)
+
+    # Check marin coefficients for stress integration
+    kappa_y = (eps_min - eps_0) * 2 / h
+
+    # We are not checking here case with uniform compression / tension, skip
+    if kappa_y == 0:
+        return
+
+    expected = (
+        [(eps_c0, 0), (eps_cu, eps_c0)],
+        [
+            (
+                fc / eps_c0 * eps_0 * (2 - eps_0 / eps_c0),
+                2 * fc * kappa_y / eps_c0 * (1 - eps_0 / eps_c0),
+                -fc * kappa_y**2 / eps_c0**2,
+            ),
+            (fc,),
+        ],
+    )
+
+    strain, coeffs = law.__marin__([eps_0, kappa_y])
+
+    assert len(strain) == len(expected[0])
+    assert len(expected[1]) == len(coeffs)
+
+    for coeff, expect in zip(coeffs, expected[1]):
+        assert len(coeff) == len(expect)
+        for i in range(len(coeff)):
+            assert math.isclose(coeff[i], expect[i])
+
+    # Check marin coefficients for modulus integration
+    expected = (
+        [(eps_c0, 0), (eps_cu, eps_c0)],
+        [
+            (
+                2 * fc / eps_c0 * (1 - eps_0 / eps_c0),
+                -2 * fc * kappa_y / eps_c0**2,
+            ),
+            (0.0,),
         ],
     )
 
