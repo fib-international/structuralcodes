3dc2a25be3eb759d9b9b9946898de2a1a2e7f326
Kristoffer Kristensen <krkris@stud.ntnu.no>
2025-03-24 17:42:14 +0100

    feat: implement shell geometry (#228)
    * Add shell reinforcement
    
    * Add shell geometry

--- structuralcodes/geometry/_shell_geometry.py
+++ structuralcodes/geometry/_shell_geometry.py
@@ -16,9 +16,56 @@ class ShellReinforcement(Geometry):
     _material: Material
     _phi: float
 
-    def __init__(self):
+    def __init__(
+        self,
+        z: float,
+        n_bars: float,
+        cc_bars: float,
+        diameter_bar: float,
+        material: Material,
+        phi: float,
+        name: t.Optional[str] = None,
+        group_label: t.Optional[str] = None,
+    ) -> None:
         """Initialize a shell reinforcement."""
-        raise NotImplementedError
+        super().__init__(name, group_label)
+
+        self._z = z
+        self._n_bars = n_bars
+        self._cc_bars = cc_bars
+        self._diameter_bar = diameter_bar
+        self._material = material
+        self._phi = phi
+
+    @property
+    def z(self) -> float:
+        """Return the reinforcement position over the thickness."""
+        return self._z
+
+    @property
+    def n_bars(self) -> float:
+        """Return the number of bars per unit width."""
+        return self._n_bars
+
+    @property
+    def cc_bars(self) -> float:
+        """Return the spacing between bars."""
+        return self._cc_bars
+
+    @property
+    def diameter_bar(self) -> float:
+        """Return the bar diameter."""
+        return self._diameter_bar
+
+    @property
+    def material(self) -> Material:
+        """Return the material of the reinforcement."""
+        return self._material
+
+    @property
+    def phi(self) -> float:
+        """Return the orientation angle of the reinforcement."""
+        return self._phi
 
 
 class ShellGeometry(Geometry):
@@ -32,16 +79,37 @@ class ShellGeometry(Geometry):
         material: Material,
         name: t.Optional[str] = None,
         group_label: t.Optional[str] = None,
-    ):
+    ) -> None:
         """Initialize a shell geometry."""
         super().__init__(name=name, group_label=group_label)
+
+        if thickness <= 0:
+            raise ValueError('Shell thickness must be positive.')
+
         self._thickness = thickness
         self._material = material
+
+        self._reinforcement = []
+
+    @property
+    def thickness(self) -> float:
+        """Return the shell thickness."""
+        return self._thickness
+
+    @property
+    def material(self) -> Material:
+        """Return the material of the shell."""
+        return self._material
+
+    @property
+    def reinforcement(self) -> t.List[ShellReinforcement]:
+        """Return all reinforcement layers."""
+        return self._reinforcement
 
     def add_reinforcement(
         self,
         reinforcement: t.Union[ShellReinforcement, t.List[ShellReinforcement]],
-    ):
+    ) -> None:
         """Add reinforcement to the shell geometry."""
         if isinstance(reinforcement, ShellReinforcement):
             self._reinforcement.append(reinforcement)
