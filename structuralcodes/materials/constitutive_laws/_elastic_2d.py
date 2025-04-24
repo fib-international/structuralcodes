ee4146eb8216a2e7c25af51fa1d3857967500936
Kristoffer K. Kristensen <krkris@stud.ntnu.no>
2025-04-24 15:06:44 +0200

    Establish the C_matrix as an internal attribute and a property + various fixes

--- structuralcodes/materials/constitutive_laws/_elastic_2d.py
+++ structuralcodes/materials/constitutive_laws/_elastic_2d.py
@@ -25,6 +25,13 @@ class Elastic2D(Elastic):
         super().__init__(E=E, name=name)
         self._nu = nu
 
+        self._C_matrix: t.Optional[ArrayLike] = None
+
+    @property
+    def E(self) -> float:
+        """Return the elastic modulus."""
+        return self._E
+
     @property
     def nu(self) -> float:
         """Return Poisson's ratio.
@@ -35,36 +42,40 @@ class Elastic2D(Elastic):
         """
         return self._nu
 
-    def get_stress(self, eps: t.Union[ArrayLike, np.ndarray]) -> np.ndarray:
+    @property
+    def C_matrix(self) -> np.ndarray:
+        """Return the 2D constitutive matrix."""
+        if self._C_matrix is None:
+            E = self.E
+            nu = self.nu
+
+            self._C_matrix = (
+                E
+                / (1 - nu**2)
+                * np.array(
+                    [
+                        [1.0, nu, 0.0],
+                        [nu, 1.0, 0.0],
+                        [0.0, 0.0, (1 - nu) / 2.0],
+                    ]
+                )
+            )
+        return self._C_matrix
+
+    def get_stress(self, eps: ArrayLike) -> np.ndarray:
         """Return a 2D stress vector [sigma_x, sigma_y, tau_xy]
         given a 2D strain vector [eps_x, epx_y, gamma_xy].
         """
-        E = self._E
-        nu = self.nu
+        eps = np.atleast_1d(eps)
+        try:
+            return self.C_matrix @ eps
+        # Sjekk at det er en ValueError
+        except ValueError as e:
+            raise ValueError(
+                'The input strain vector must have a length of 3.'
+            ) from e
 
-        eps = np.array(eps, dtype=float).reshape(3)
-
-        factor = E / (1 - nu**2)
-        C = factor * np.array(
-            [
-                [1.0, nu, 0.0],
-                [nu, 1.0, 0.0],
-                [0.0, 0.0, (1 - nu) / 2.0],
-            ]
-        )
-        return eps @ C
-
-    def get_tangent(self) -> np.ndarray:
+    def get_tangent(self, *args, **kwargs) -> np.ndarray:
         """Return the 2D tangent stiffness matrix."""
-        E = self._E
-        nu = self.nu
-
-        factor = E / (1 - nu**2)
-        C = factor * np.array(
-            [
-                [1.0, nu, 0.0],
-                [nu, 1.0, 0.0],
-                [0.0, 0.0, (1 - nu) / 2.0],
-            ]
-        )
-        return C  # noqa: RET504
+        del args, kwargs
+        return self.C_matrix
