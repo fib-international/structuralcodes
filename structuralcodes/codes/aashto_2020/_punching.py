5b65283fd548c246787bfe2230446eff11a909b8
Samc-3 <sam.s@tamu.edu>
2025-06-30 13:03:34 +0200

    Shear punching file for AASHTO

---
+++ structuralcodes/codes/aashto_2020/_punching.py
@@ -0,0 +1,99 @@
+import math
+
+
+def bo_edge(W: float, L: float, df: float) -> float:
+    """Determines the critical perimeter for a bearing on the edge of the beam.
+
+    AASHTO LRFD 2020 9th Edition, Eq. (5.8.4.4-4)
+
+    Args:
+        W (float) = width of the bearing plate or pad (in)
+        L (float) = length of the bearing pad (in)
+        df (float) = distance from the top of the ledge to the bottom
+        longitudinal reinforcement (in)
+
+    Returns:
+        The critical perimeter for a bearing on the edge of the beam in (in)
+
+    Raises:
+        ValueError: If W is less than 0
+        ValueError: If L is less than 0
+        ValueError: If df is less than 0
+    """
+    if W < 0:
+        raise ValueError(f'W={W} cannot be less than 0')
+    if L < 0:
+        raise ValueError(f'L={L} cannot be less than 0')
+    if df < 0:
+        raise ValueError(f'df={df} cannot be less than 0')
+
+    return W + 2 * L + 2 * df
+
+
+def b0_corner(W: float, L: float, df: float, c: float) -> float:
+    """Determines the critical perimeter for a bearing in the corner of
+    the beam.
+
+    AASHTO LRFD 2020 9th Edition, Eq. (5.8.4.4-5)
+
+    Args:
+        W (float) = width of the bearing plate or pad (in)
+        L (float) = length of the bearing pad (in)
+        df (float) = distance from the top of the ledge to the bottom
+        longitudinal reinforcement (in)
+        c (float) = spacing from the centerline of the bearing to the end of
+        beam ledge (in)
+
+    Returns:
+        The critical perimeter for a bearing in the corner of the beam in (in)
+
+    Raises:
+        ValueError: If W is less than 0
+        ValueError: If L is less than 0
+        ValueError: If df is less than 0
+        ValueError: If c is less than 0
+    """
+    if W < 0:
+        raise ValueError(f'W={W} cannot be less than 0')
+    if L < 0:
+        raise ValueError(f'L={L} cannot be less than 0')
+    if df < 0:
+        raise ValueError(f'df={df} cannot be less than 0')
+    if c < 0:
+        raise ValueError(f'c={c} cannot be less than 0')
+
+    return 0.5 * W + L + df + c
+
+
+def bo_interior(W: float, L: float, df: float) -> float:
+    """Determines the critical perimeter for a bearing in the interior of
+    the beam.
+
+    Was interpolated based on the edge bearing formula
+
+    Args:
+        W (float) = width of the bearing plate or pad (in)
+        L (float) = length of the bearing pad (in)
+        df (float) = distance from the top of the ledge to the bottom
+        longitudinal reinforcement (in)
+
+    Returns:
+        The critical perimeter for a bearing in the interior of the beam
+        in (in)
+
+    Raises:
+        ValueError: If W is less than 0
+        ValueError: If L is less than 0
+        ValueError: If df is less than 0
+    """
+    if W < 0:
+        raise ValueError(f'W={W} cannot be less than 0')
+    if L < 0:
+        raise ValueError(f'L={L} cannot be less than 0')
+    if df < 0:
+        raise ValueError(f'df={df} cannot be less than 0')
+
+    return 2 * W + 2 * L + 4 * df
+
+
+v = math.sqrt(5)
