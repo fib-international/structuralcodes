2e4dd16f50541e7e3bfe9b3e73e0894f1e8074bd
saschabuehrle <sascha.buehrle@gmail.com>
2026-03-22 13:13:56 +0100

    fix: guard perimeter computation for MultiPolygon geometries (fixes #329)

--- structuralcodes/sections/_generic.py
+++ structuralcodes/sections/_generic.py
@@ -150,8 +150,8 @@ class GenericSectionCalculator(SectionCalculator):
             warnings.warn(
                 'Perimiter computation for a multi polygon is not defined.'
             )
-
-        gp.perimeter = polygon.exterior.length
+        else:
+            gp.perimeter = polygon.exterior.length
 
         # Computation of area: this is taken directly from shapely
         gp.area = self.section.geometry.area
