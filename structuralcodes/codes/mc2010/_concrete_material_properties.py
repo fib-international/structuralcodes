b97a495609c8548667d720b2114939728d6a83ac
Morten Engen <morten.engen@multiconsult.no>
2026-04-21 16:00:25 +0200

    Add design tensile strength for MC2010

--- structuralcodes/codes/mc2010/_concrete_material_properties.py
+++ structuralcodes/codes/mc2010/_concrete_material_properties.py
@@ -218,6 +218,26 @@ def fcd(fck: float, alpha_cc: float = 1.0, gamma_c: float = 1.5) -> float:
     return abs(alpha_cc) * abs(fck) / abs(gamma_c)
 
 
+def fctd(fctk: float, alpha_ct: float = 1.0, gamma_c: float = 1.5) -> float:
+    """The design tensile strength of concrete.
+
+    Defined in fib Model Code 2010 (2013), Eq. 7.2-12.
+
+    Args:
+        fctk (float): The characteristic tensile strength in MPa.
+
+    Keyword Args:
+        alpha_ct (float): A factor for considering long-term effects on the
+            strength, and effects that arise from the way the load is applied.
+            Default value 1.0.
+        gamma_c (float): The partial factor of concrete. Default value 1.5.
+
+    Returns:
+        float: The design tensile strength of concrete in MPa.
+    """
+    return abs(alpha_ct) * abs(fctk) / abs(gamma_c)
+
+
 def eps_c1(fck: float) -> float:
     """The strain at maximum compressive stress of concrete (fcm) for the
     Sargin constitutive law.
